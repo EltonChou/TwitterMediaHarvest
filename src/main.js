dbf95b09af8cb1eb639ec8b2b263650ec6000815
Elton Chou <ie9181124@gmail.com>
2018-06-09 01:56:09 +0900

    BETA

--- src/main.js
+++ src/main.js
@@ -1,50 +1,65 @@
 require ('./assets/css/style.sass')
-
-const body = document.querySelector('body')
-const stream = document.querySelectorAll("#stream-items-id")[0]
 const galleryTweet = document.querySelector('.GalleryTweet')
 const permalink = document.querySelector('.PermalinkOverlay-body')
-const title = document.querySelector('title')
+/*
+ * Append button to status of first-group
+ */
 
 // TODO: When saving image from right-click menu, change the target link to *:orig
 // TODO: onDetermineFilename change filename
-// TODO: Export observer setting
-// TODO: Permalink mutation-observe
-// TODO: When route change need to start observer again
-observeElement( title, mutations => {
-    for ( const mutation of mutations ) {
-    }
-}, {childList: true})
+// TODO: Export observer setting (??)
 
-observeElement( permalink, mutations => {
-    for (const mutation of mutations) {
-        console.log(mutation)
-        if (mutation.addedNodes.length) {
-            for( const inner of mutation.target.parentElement.querySelectorAll('.permalink-inner') ) {
-                appendOrigClickTo(inner)
+function observeTitle() {
+    const title = document.querySelector('title')
+    const body = document.querySelector('body')
+    // const title = document.querySelector('#sr-event-log')
+    observeElement( title, mutations => {
+        for ( const mutation of mutations ) {
+            console.log(mutation)
+        }
+        if( body.classList.contains("overlay-enabled")) {
+            return false
+        } else {
+            reload()
+        }
+    }, { childList: true, subtree: true })
+}
+
+function observePermalink() {
+    observeElement( permalink, mutations => {
+        for (const mutation of mutations) {
+            console.log(mutation)
+            if (mutation.addedNodes.length) {
+                for( const inner of mutation.target.parentElement.querySelectorAll('.permalink-inner') ) {
+                    appendOrigClickTo(inner)
+                }
             }
         }
-    }
-}, {childList: true})
+    }, {childList: true})
+}
 
-observeElement( galleryTweet, mutations => {
-    for (const mutation of mutations) {
-        if (mutation.addedNodes.length) {
-            insertOrigClickBeforeMore(mutation.target.parentElement)
-        }
-    }
-}, {childList: true})
-
-observeElement( stream, mutations => {
-    for (const mutation of mutations) {
-        for (const addedNode of mutation.addedNodes) {
-            if (addedNode.querySelector(".AdaptiveMedia-singlePhoto")) {
-                appendOrigClickTo(addedNode)
+function observeGallery() {    
+    observeElement( galleryTweet, mutations => {
+        for (const mutation of mutations) {
+            if (mutation.addedNodes.length) {
+                insertOrigClickBeforeMore(mutation.target.parentElement)
             }
         }
-    }
-}, {childList: true})
+    }, {childList: true})   
+}
 
+function observeStream() {
+    const stream = document.querySelectorAll("#stream-items-id")[0]   
+    observeElement( stream, mutations => {
+        for (const mutation of mutations) {
+            for (const addedNode of mutation.addedNodes) {
+                if (addedNode.querySelector(".AdaptiveMedia-singlePhoto")) {
+                    appendOrigClickTo(addedNode)
+                }
+            }
+        }
+    }, {childList: true})    
+}
 
 /**
  * MutationObserver
@@ -70,8 +85,34 @@ function observeElement( element, callback, options = {childList: true} ) {
  * Create appended button
  * @param {Object} DOMElement element
  */
-function appendOrigClickTo(element) { 
-    element.getElementsByClassName("ProfileTweet-actionList js-actions")[0].appendChild(origClickFor(element))
+function appendOrigClickTo(element) {
+    if (!element || element.getAttribute('hasBeenAppended')) {
+        return false
+    } else {
+        try {
+            element.setAttribute('hasBeenAppended', true)
+            element.getElementsByClassName("ProfileTweet-actionList js-actions")[0].appendChild(origClickFor(element))
+        } catch(err){
+            console.error(err)
+        }
+    }
+}
+
+function validateElement(element) {
+    return element && !element.getAttribute('hasBeenAppended')
+}
+
+function insertOrigClickBeforeMore(element) {
+    if (!element) {
+        return false
+    }
+    if (element.getAttribute('hasBeenAppended')){
+        return false
+    } else {
+        element.setAttribute('hasBeenAppended', true)
+    const el = element.getElementsByClassName("ProfileTweet-actionList js-actions")[0]
+    el.insertBefore(origClickFor(element), el.childNodes[9])
+    }
 }
 
 function getImageUrl (element) {
@@ -80,45 +121,40 @@ function getImageUrl (element) {
         : element.getElementsByClassName("media-image")[0].getAttribute("src")
 }
 
-function insertOrigClickBeforeMore(element) {
-    const el = element.getElementsByClassName("ProfileTweet-actionList js-actions")[0]
-    el.insertBefore(origClickFor(element), el.childNodes[9])
-}
-
+/**
+ * 
+ * @param {*} element 
+ */
 function origClickFor(element) {
     const imageUrl = getImageUrl(element).split(':')
     const div = document.createElement('div')
-    const button = document.createElement('div')
-    const a = document.createElement('a')
+    const button = document.createElement('button')
+    const iconContainer = document.createElement('div')
+    const span = document.createElement('span')
     const dataUrl = imageUrl[0] + ":" + imageUrl[1] + ":orig"
     const dataName = imageUrl[1].split("/")[4]
-    a.innerText = "Kappa"
-    a.setAttribute("id", "lazyOrigDownload")
-    a.setAttribute('class', "IconContainer")
+    iconContainer.setAttribute("id", "lazyOrigDownload")
+    iconContainer.setAttribute('class', "IconContainer")
+    span.setAttribute('class', "Icon")
+    span.innerText = "Kappa"
     div.classList.add('lazyDownContainer')
     button.classList.add('ProfileTweet-actionButton')
     button.setAttribute("data-url", dataUrl)
     button.setAttribute("data-name", dataName)
-    button.appendChild(a)
+    iconContainer.appendChild(span)
+    button.appendChild(iconContainer)
+    div.appendChild(button)
     button.addEventListener("click", function(){
         downloadImage(this.getAttribute("data-url"), this.getAttribute("data-name"))
     })
-    div.appendChild(button)
     return div
 }
 
-/**
- * Append button to status of first-group
- */
-function init(){
+function piorneer() {
     const streamItems = document.getElementsByClassName("js-stream-item stream-item stream-item")
     for ( const streamItem of streamItems) {
-        if( streamItem.getElementsByClassName("AdaptiveMedia-singlePhoto").length ){
-            try {
-                appendOrigClickTo(streamItem)
-            } catch(err){
-                console.log(streamItem)
-            }
+        if( streamItem.getElementsByClassName("AdaptiveMedia-singlePhoto").length && !streamItem.getAttribute('hasBeenAppended') ){
+            appendOrigClickTo(streamItem)
         }
     }
 }
@@ -134,6 +170,19 @@ function downloadImage(url, filename) {
         url: url,
         filename: filename
     });
+}
+
+function init() {
+    piorneer()
+    observeTitle()
+    observeStream()
+    observeGallery()
+    observePermalink()
+}
+
+function reload() {
+    piorneer()
+    observeStream()
 }
 
 init()
