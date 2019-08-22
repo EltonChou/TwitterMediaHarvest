09ecb962dfc31e2d5ab3c8c71c0d455c77ff022c
EltonChou <ie9181124@gmail.com>
2019-08-23 01:25:36 +0800

    typo

--- src/background.js
+++ src/background.js
@@ -36,7 +36,7 @@ const initHeader = token => {
     'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
   )
   header.append('cache-control', 'no-cache')
-  header.append('x-twitter-active-user', 'yegits')
+  header.append('x-twitter-active-user', 'yes')
   header.append('x-twitter-auth-type', 'OAuth2Session')
   header.append('x-csrf-token', token)
   return header
