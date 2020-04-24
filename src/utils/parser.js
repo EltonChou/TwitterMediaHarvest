8baab0bed6d9f1856a3fade3649f212375d39b51
EltonChou <ie9181124@gmail.com>
2020-04-25 06:46:30 +0800

    [HOTFIX] hotfix for #1]

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,30 +1,9 @@
-import path from 'path'
 import select from 'select-dom'
 import { isArticlePhotoMode, isArticleStatusMode } from './checker'
 
 const idFeature = /(?:status\/)(\d+)/
 const screenNameFeature = /(?<=@)\S+/
 const urlScreenNameFeature = /(?<=\/)\S+(?=\/status)/
-
-/**
- * Parse file information from url
- *
- * @param {string} url
- * @returns {object}
- */
-export const parseFileFromUrl = url => {
-  const src = new URL(url)
-  const srcPathname = src.pathname
-
-  if (path.extname(srcPathname) !== '.mp4')
-    src.searchParams.append('name', 'orig')
-  const srcBasename = path.basename(srcPathname)
-
-  return {
-    src: src.href,
-    basename: srcBasename,
-  }
-}
 
 const parseScreeNameFromUserAccount = article => {
   const userAccount = select('[data-testid="tweet"] [dir="ltr"]', article)
@@ -46,14 +25,19 @@ const parseScreeNameFromUserAccount = article => {
  * @param {Element} article A valid tweet element.
  * @returns {tweetInfo}
  */
+// FIXME: some tweet will cause null time error
 export const parseTweetInfo = article => {
-  const time = select('time', article)
-  const magicLink =
+  let magicLink =
     isArticlePhotoMode(article) || isArticleStatusMode(article)
       ? window.location.pathname
-      : time.parentNode.getAttribute('href')
+      : null
 
-  const tweetId = magicLink.match(idFeature)[1]
+  const time = select('time', article)
+  if (time) {
+    magicLink = time.parentNode.getAttribute('href')
+  } else return void 0
+
+  const tweetID = magicLink.match(idFeature)[1]
 
   const screenName = isArticlePhotoMode(article)
     ? magicLink.match(urlScreenNameFeature)[0]
@@ -61,6 +45,6 @@ export const parseTweetInfo = article => {
 
   return {
     screenName: screenName,
-    tweetId: tweetId,
+    tweetID: tweetID,
   }
 }
