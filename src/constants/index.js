b65cb6f9c14f019dce1948a7ddd439b7d3965d85
EltonChou <ie9181124@gmail.com>
2020-04-25 06:45:39 +0800

    [ADD] chrome storage fetch filename setting

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,3 +3,5 @@ export const TWITTER_AUTH_TOKEN =
   'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
 export const USER_AGENT =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.'
+export const CHROME_STORAGE_DEFAULT_FILENAME_PATTERN_OBJECT_STRING =
+  '{"account": true, "serial": "file_name"}'
--- src/lib/chromeApi.js
+++ src/lib/chromeApi.js
@@ -1,16 +1,29 @@
+import {
+  DEFAULT_DIRECTORY,
+  CHROME_STORAGE_DEFAULT_FILENAME_PATTERN_OBJECT_STRING,
+} from '../constants'
+
 /**
  * Fetch data from chrome storage.
  *
+ * @async
  * @param {(string|string[]|Object)} any
  * @returns {promise}
  */
-const fetchStorage = any =>
+export const fetchStorage = any =>
   new Promise(resolve => {
     // eslint-disable-next-line no-undef
     chrome.storage.sync.get(any, result => resolve(result))
   })
 
-const setStorage = obj =>
+/**
+ * Set data to1 chrome storage.
+ *
+ * @async
+ * @param {Object} obj
+ * @returns {promise}
+ */
+export const setStorage = obj =>
   new Promise(resolve => {
     // eslint-disable-next-line no-undef
     chrome.storage.sync.set(obj, () => resolve(obj))
@@ -19,13 +32,44 @@ const setStorage = obj =>
 /**
  * Fetch chrome cookie
  *
+ * @async
  * @param {Object} target
  * @returns {promise}
  */
-const fetchCookie = target =>
+export const fetchCookie = target =>
   new Promise(resolve => {
     // eslint-disable-next-line no-undef
     chrome.cookies.get(target, cookie => resolve(cookie))
   })
 
-export { setStorage, fetchCookie, fetchStorage }
+/**
+ * serial would be `order` or `file_name`
+ * @typedef patternSetting
+ * @type {Object}
+ * @property {Boolean} account
+ * @property {String} serial - `order` or `file_name`
+ */
+/**
+ * @typedef fileNameSetting
+ * @type {Object}
+ * @property {String} directory
+ * @property {patternSetting} filename_pattern
+ *
+ */
+/**
+ * Fetch settings
+ *
+ * @async
+ * @return {Promise<fileNameSetting>}
+ */
+export const fetchFileNameSetting = async () => {
+  const defaultSetting = {
+    directory: DEFAULT_DIRECTORY,
+    filename_pattern: CHROME_STORAGE_DEFAULT_FILENAME_PATTERN_OBJECT_STRING,
+  }
+
+  const setting = await fetchStorage(defaultSetting)
+  setting.filename_pattern = JSON.parse(setting.filename_pattern)
+
+  return setting
+}
