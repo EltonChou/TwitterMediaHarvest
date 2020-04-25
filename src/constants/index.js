efb7c2e7deb89889813a4db7d73621ac5fb2e791
EltonChou <ie9181124@gmail.com>
2020-04-25 10:01:45 +0800

    [ADD] add some storage default value

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,4 +4,4 @@ export const TWITTER_AUTH_TOKEN =
 export const USER_AGENT =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.'
 export const CHROME_STORAGE_DEFAULT_FILENAME_PATTERN_OBJECT_STRING =
-  '{"account": true, "serial": "file_name"}'
+  '{"account": true, "serial": "order"}'
