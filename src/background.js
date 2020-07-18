e8c899a7ea4ab2e3738b8597d9d79e2b10a6ccf9
EltonChou <ie9181124@gmail.com>
2020-07-18 17:33:35 +0800

    [REFINE] refine the code

--- src/background.js
+++ src/background.js
@@ -17,40 +17,48 @@ chrome.runtime.onInstalled.addListener(async details => {
   openOptionsPage()
 })
 
-// eslint-disable-next-line no-undef
-chrome.runtime.onMessage.addListener(
-  async request => await downloadMedias(request)
-)
-
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+chrome.runtime.onMessage.addListener(processRequest)
 chrome.browserAction.onClicked.addListener(openOptionsPage)
+/* eslint-enable no-undef */
 
 /**
  * Trigger browser-download
- *
- * @function downloadMedias
- * @param {JSON} info twitter information
+ * @typedef {import('./lib/TwitterMediaFile').tweetInfo} tweetInfo
+ * @param {tweetInfo} tweetInfo twitter information
+ * @returns {void}
  */
-async function downloadMedias(info) {
+async function processRequest(tweetInfo) {
   // eslint-disable-next-line no-undef
   let { value } = await fetchCookie({ url: 'https://twitter.com', name: 'ct0' })
+  const twitterMedia = new MediaTweet(tweetInfo.tweetId, value)
 
-  const twitterMedia = new MediaTweet(info.tweetId, value)
-  const mediaList = await twitterMedia.fetchMediaList()
+  twitterMedia
+    .fetchMediaList()
+    .then(mediaList => downloadMedias(mediaList, tweetInfo))
+}
+
+/**
+ * @param {Array<String>} mediaList
+ * @param {tweetInfo} tweetInfo
+ * @returns {void}
+ */
+async function downloadMedias(mediaList, tweetInfo) {
   const setting = await fetchFileNameSetting()
+  const isPassToAria2 = JSON.parse(
+    localStorage.getItem(LOCAL_STORAGE_KEY_ARIA2)
+  )
+  const downloadTool = isPassToAria2 ? 'aria2' : 'browser'
 
   for (const [index, value] of mediaList.entries()) {
-    const mediaFile = new TwitterMediaFile(info, value, index)
+    const mediaFile = new TwitterMediaFile(tweetInfo, value, index)
+    const config = mediaFile.makeDownloadConfigBySetting(setting, downloadTool)
 
-    if (JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_ARIA2))) {
-      const config = mediaFile.makeDownloadConfigBySetting(setting, 'aria2')
-      // eslint-disable-next-line no-undef
-      chrome.runtime.sendMessage(ARIA2_ID, config)
-    } else {
-      const config = mediaFile.makeDownloadConfigBySetting(setting, 'browser')
-      // eslint-disable-next-line no-undef
-      chrome.downloads.download(config)
-    }
+    /* eslint-disable no-undef */
+    isPassToAria2
+      ? chrome.runtime.sendMessage(ARIA2_ID, config)
+      : chrome.downloads.download(config)
+    /* eslint-enable no-undef */
   }
 }
 
@@ -61,8 +69,8 @@ function openOptionsPage() {
 
 /* eslint-disable no-console */
 function showUpdateMessage(current, prev) {
+  console.info('The extension has been updated.')
   console.info('Previous version:', prev)
   console.info('Current version:', current)
-  console.info('The extension has been updated.')
 }
 /* eslint-enable no-console */
