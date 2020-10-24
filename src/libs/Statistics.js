232286129f9e620d29b21b232011614e29090239
EltonChou <ie9181124@gmail.com>
2020-10-25 06:46:50 +0800

    [REFINE] move Statistics class to libs

---
+++ src/libs/Statistics.js
@@ -0,0 +1,36 @@
+import { addStatisticsCount, getStatisticsCount } from '../helpers/storageHelper'
+
+const statisticsKey = Object.freeze({
+  successDownloadCount: 'successDownloadCount',
+  failedDownloadCount: 'failedDownloadCount',
+  errorCount: 'errorCount',
+})
+
+
+export default class Statistics {
+  static async addSuccessDownloadCount() {
+    await addStatisticsCount(statisticsKey.successDownloadCount)
+  }
+
+  static async addFailedDownloadCount() {
+    await addStatisticsCount(statisticsKey.failedDownloadCount)
+  }
+
+  static async addErrorCount() {
+    await addStatisticsCount(statisticsKey.errorCount)
+  }
+
+  static async getSuccessDownloadCount() {
+    const count = await getStatisticsCount(statisticsKey.successDownloadCount)
+    return count
+  }
+  static async getFailedDownloadCount() {
+    const count = await getStatisticsCount(statisticsKey.failedDownloadCount)
+    return count
+  }
+
+  static async getErrorCount() {
+    const count = await getStatisticsCount(statisticsKey.errorCount)
+    return count
+  }
+}
