ae37730d8684ca28aabda746003abd51ef593b03
Elton Chou <plscd748@gmail.com>
2021-12-26 21:45:44 +0800

    add: twitterdeck observer

---
+++ src/libs/TwitterDeckObserver.js
@@ -0,0 +1,49 @@
+import select from 'select-dom'
+import makeHarvester from '../core'
+import observeElement from '../utils/observer'
+
+export default class TwitterDeckObserver {
+  /** @returns {void} */
+  observeRoot() {
+    /** @type {MutationCallback} */
+    const rootCallback = (_, this_observer) => {
+      if (select.exists('.app-columns')) {
+        // console.log('huja')
+        this.observeContent()
+        this_observer.disconnect()
+      }
+    }
+
+    /** @type {MutationObserverInit} */
+    const options = {
+      childList: true,
+    }
+
+    observeElement('.application', rootCallback, options)
+  }
+
+  /** @returns {void} */
+  observeContent() {
+    /** @type {MutationCallback} */
+    const contentCallback = mutations => {
+      for (const mutation of mutations) {
+        for (const addedNode of mutation.addedNodes) {
+          if (select.exists('.media-preview', addedNode)) {
+            makeHarvester(addedNode)
+          }
+        }
+      }
+    }
+
+    /** @type {MutationObserverInit} */
+    const options = {
+      childList: true,
+    }
+
+    const tweetContainers = select.all('.chirp-container')
+
+    for (const tweetContainer of tweetContainers) {
+      observeElement(tweetContainer, contentCallback, options)
+    }
+  }
+}
