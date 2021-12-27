f2ae5607f9047bdc386b5a845e866fe2154fac4e
Elton Chou <plscd748@gmail.com>
2021-12-28 04:18:14 +0800

    add: columnContainer observer

--- src/libs/TwitterDeckObserver.js
+++ src/libs/TwitterDeckObserver.js
@@ -11,35 +11,111 @@ const deckStreamHasMedia = addedNode =>
   select.exists('.media-preview', addedNode) &&
   !select.exists('.quoted-tweet', addedNode)
 
+const observerDetailReplies = replies => {
+  /** @type {MutationObserverInit} */
+  const options = {
+    childList: true,
+  }
+
+  /** @type {MutationCallback} */
+  const repliesCallback = mutations => {
+    for (const mutation of mutations) {
+      for (const addedNode of mutation.addedNodes) {
+        if (deckStreamHasMedia(addedNode)) {
+          makeHarvester(addedNode)
+        }
+      }
+    }
+  }
+
+  observeElement(replies, repliesCallback, options)
+}
+
+const observeModal = () => {
+  /** @type {MutationCallback} */
+  const modalCallback = mutations => {
+    for (const mutation of mutations) {
+      if (mutation.addedNodes) {
+        const article = select('.tweet', mutation.addedNodes[0])
+        makeHarvester(article)
+      }
+    }
+  }
+
+  /** @type {MutationObserverInit} */
+  const options = {
+    childList: true,
+  }
+
+  observeElement('#open-modal', modalCallback, options)
+}
+
+const observeColumns = () => {
+  const columnContainer = select('.app-columns')
+
+  /** @type {MutationObserverInit} */
+  const observerOptions = {
+    childList: true,
+  }
+
+  observeElement(
+    columnContainer,
+    mutations => {
+      for (const mutation of mutations) {
+        for (const addedNode of mutation.addedNodes) {
+          console.log(addedNode)
+        }
+      }
+    },
+    observerOptions
+  )
+}
+
 export default class TwitterDeckObserver {
   /** @returns {void} */
   observeRoot() {
-    /** @type {MutationCallback} */
-    const rootCallback = (_, this_observer) => {
-      if (select.exists('.app-columns')) {
-        this.observeContent()
-        this.observeModal()
-        this_observer.disconnect()
-      }
-    }
-
     /** @type {MutationObserverInit} */
     const options = {
       childList: true,
     }
 
-    observeElement('.application', rootCallback, options)
+    observeElement(
+      '.application',
+      (_, this_observer) => {
+        if (select.exists('.app-columns')) {
+          this.initObservers()
+          observeColumns()
+          this_observer.disconnect()
+        }
+      },
+      options
+    )
   }
 
   /** @returns {void} */
-  observeContent() {
+  initObservers() {
+    const tweetContainers = select.all('.chirp-container')
+    const tweetDetails = select.all('.column-detail')
+
+    for (const tweetDetail of tweetDetails) {
+      this.observeDetail(tweetDetail)
+    }
+
+    for (const tweetContainer of tweetContainers) {
+      this.observeStreamContainer(tweetContainer)
+    }
+
+    observeModal()
+  }
+
+  observeStreamContainer(streamContainer) {
     /** @type {MutationObserverInit} */
     const options = {
       childList: true,
     }
 
     /** @type {MutationCallback} */
-    const contentCallback = mutations => {
+    const streamCallback = mutations => {
       for (const mutation of mutations) {
         for (const addedNode of mutation.addedNodes) {
           if (deckStreamHasMedia(addedNode)) {
@@ -49,14 +125,14 @@ export default class TwitterDeckObserver {
       }
     }
 
-    const repliesCallback = mutations => {
-      for (const mutation of mutations) {
-        for (const addedNode of mutation.addedNodes) {
-          if (deckStreamHasMedia(addedNode)) {
-            makeHarvester(addedNode)
-          }
-        }
-      }
+    observeElement(streamContainer, streamCallback, options)
+  }
+
+  observeDetail(tweetDetail) {
+    /** @type {MutationObserverInit} */
+    const detailOptions = {
+      childList: true,
+      subtree: true,
     }
 
     /** @type {MutationCallback} */
@@ -73,43 +149,9 @@ export default class TwitterDeckObserver {
           replies = select('.replies-after', mutation.target)
         }
       }
-      observeElement(replies, repliesCallback, options)
+      observerDetailReplies(replies)
     }
 
-    const tweetContainers = select.all('.chirp-container')
-    const tweetDetails = select.all('.column-detail')
-
-    //FIXME: monkey patch
-    /** @type {MutationObserverInit} */
-    const detailOptions = {
-      childList: true,
-      subtree: true,
-    }
-    for (const tweetDetail of tweetDetails) {
-      observeElement(tweetDetail, detailCallback, detailOptions)
-    }
-
-    for (const tweetContainer of tweetContainers) {
-      observeElement(tweetContainer, contentCallback, options)
-    }
-  }
-
-  observeModal() {
-    /** @type {MutationCallback} */
-    const modalCallback = mutations => {
-      for (const mutation of mutations) {
-        if (mutation.addedNodes) {
-          const article = select('.tweet', mutation.addedNodes[0])
-          makeHarvester(article)
-        }
-      }
-    }
-
-    /** @type {MutationObserverInit} */
-    const options = {
-      childList: true,
-    }
-
-    observeElement('#open-modal', modalCallback, options)
+    observeElement(tweetDetail, detailCallback, detailOptions)
   }
 }
