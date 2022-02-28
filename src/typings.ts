d9e77fc1f146ec1c78e3f4f1f684d951493d03c7
Elton Chou <plscd748@gmail.com>
2022-03-01 03:30:27 +0800

    add: add typings

---
+++ src/typings.ts
@@ -0,0 +1,63 @@
+import { LOCAL_STORAGE_KEY_ARIA2 } from './constants'
+
+export abstract class TwitterMediaHarvestObserver {
+  public abstract observeRoot(): void
+}
+
+export interface TweetInfo {
+  screenName: string
+  tweetId: string
+}
+
+export interface FilenamePatternOption {
+  account: boolean
+  serial: 'order' | 'file_name'
+}
+
+export interface FilenameSetting {
+  directory: string
+  no_subdirectory: boolean
+  filename_pattern: FilenamePatternOption
+}
+
+export enum DownloadMode {
+  Aria2 = 'aria2',
+  Browser = 'browser',
+}
+
+export interface Aria2DownloadOption {
+  url: string
+  filename: string
+  referrer: string
+  options?: object
+}
+
+export type TweetMode = 'photo' | 'status' | 'stream'
+
+export enum StatisticsKey {
+  SuccessDownloadCount = 'successDownloadCount',
+  FailedDownloadCount = 'failedDownloadCount',
+  ErrorCount = 'errorCount',
+}
+
+export type LOCAL_STORAGE_KEY_ARIA2 = 'enableAria2'
+
+export interface LocalStorageInitialData {
+  [StatisticsKey.SuccessDownloadCount]: number
+  [StatisticsKey.FailedDownloadCount]: number
+  [StatisticsKey.ErrorCount]: number
+  [LOCAL_STORAGE_KEY_ARIA2]: boolean
+}
+
+export interface DownloadRecord {
+  info: TweetInfo
+  config: chrome.downloads.DownloadOptions
+}
+
+export type DownloadRecordId = `dl_${number}`
+
+export type FetchErrorReason = {
+  status: number
+  title: string
+  message: string
+}
