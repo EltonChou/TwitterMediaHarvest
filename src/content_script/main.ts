32a79726932fbc4bc6022da17d606980166a6098
Elton Chou <plscd748@gmail.com>
2022-03-07 04:48:54 +0800

    add: add keyboard control download, simple but workable

--- src/content_script/main.ts
+++ src/content_script/main.ts
@@ -1,7 +1,21 @@
 import './main.sass'
+import select from 'select-dom'
 import TwitterMediaObserver from './observers/TwitterMediaObserver'
 import TwitterDeckObserver from './observers/TwitterDeckObserver'
 import { isTweetDeck, isTwitter } from './utils/checker'
+
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'd') {
+    const currentFocusing = select('[data-focusvisible-polyfill]')
+    if (currentFocusing) {
+      const tweetCanBeHarvested = currentFocusing.closest('[data-harvest-appended]')
+      if (tweetCanBeHarvested) {
+        const harvesterButton = select('.harvester', tweetCanBeHarvested)
+        harvesterButton.click()
+      }
+    }
+  }
+})
 
 let observer: HarvestObserver = undefined
 
