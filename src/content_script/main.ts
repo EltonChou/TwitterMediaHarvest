0b0f6a636a1e4cd4b19b12ebef9438c4981cdb61
Elton Chou <plscd748@gmail.com>
2022-03-10 08:28:00 +0800

    change: use kea code instead of key

--- src/content_script/main.ts
+++ src/content_script/main.ts
@@ -7,17 +7,17 @@ import { isTweetDeck } from './utils/checker'
 let currentFocusing: Element = document.activeElement
 const observer: HarvestObserver = isTweetDeck() ? new TwitterDeckObserver() : new TwitterMediaObserver()
 
-const getDownloadKey = () => isTweetDeck() ? 'o' : 'd'
+const getDownloadKeyCode = () => isTweetDeck() ? 'KeyO' : 'KeyD'
 
 window.addEventListener('keydown', (e) => {
-  if (e.key === getDownloadKey() && e.target instanceof Element) {
+  if (e.code === getDownloadKeyCode() && e.target instanceof Element) {
     currentFocusing = isTweetDeck() ? select('.is-selected-tweet') : e.target
   }
 })
 
 window.addEventListener('keyup', (e) => {
   const buttonQuery = isTweetDeck() ? '.deck-harvester' : '.harvester'
-  if (e.key === getDownloadKey()) {
+  if (e.code === getDownloadKeyCode()) {
     if (e.target instanceof Element && currentFocusing) {
       const tweetCanBeHarvested = currentFocusing.closest('[data-harvest-appended]')
       if (tweetCanBeHarvested) {
