64be1513923c83fda3eb524d837dcb034576b017
Elton Chou <plscd748@gmail.com>
2022-06-13 02:07:30 +0800

    move MediaDownloader to libs

--- src/backend/sw.ts
+++ src/backend/sw.ts
@@ -1,8 +1,7 @@
 import * as Sentry from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
-import { ARIA2_ID } from '../constants'
 import Statistics from './libs/Statistics'
-import TwitterMediaFile, { DownloadMode } from './libs/TwitterMediaFile'
+import MediaDownloader from './libs/MediaDownloader'
 import { fetchMediaList } from './libs/MediaTweet'
 import {
   getExtensionId,
@@ -17,10 +16,8 @@ import DownloadRecordUtil from './utils/DownloadRecordUtil'
 import {
   downloadItemRecorder,
   fetchDownloadItemRecord,
-  fetchFileNameSetting,
   fetchTwitterCt0Cookie,
   initStorage,
-  isEnableAria2,
   migrateStorage,
   removeDownloadItemRecord,
 } from './helpers/storageHelper'
@@ -157,48 +154,6 @@ chrome.notifications.onButtonClicked.addListener(
 // @ts-expect-error lul
 chrome.action.onClicked.addListener(openOptionsPage)
 
-class MediaDownloader {
-  public tweetInfo: TweetInfo
-  public filenameSetting: FilenameSetting
-  public isPassToAria2: boolean
-  public mode: DownloadMode
-  private record_config: DownloadItemRecorder
-
-  constructor(
-    tweetInfo: TweetInfo,
-    filenameSetting: FilenameSetting,
-    isPassToAria2: boolean
-  ) {
-    this.tweetInfo = tweetInfo
-    this.filenameSetting = filenameSetting
-    this.isPassToAria2 = isPassToAria2
-    this.mode = this.isPassToAria2 ? DownloadMode.Aria2 : DownloadMode.Browser
-    this.record_config = downloadItemRecorder(tweetInfo)
-  }
-
-  static async build(tweetInfo: TweetInfo) {
-    const fileNameSettings = await fetchFileNameSetting()
-    const isPassToAria2 = await isEnableAria2()
-    return new MediaDownloader(tweetInfo, fileNameSettings, isPassToAria2)
-  }
-
-  downloadMedias(mediaList: string[]) {
-    mediaList.forEach((value: string, index: number) => {
-      if (!TwitterMediaFile.isValidFileUrl(value)) throw new Error(`Invalid url: ${value}`)
-
-      const mediaFile = new TwitterMediaFile(this.tweetInfo, value, index)
-      const config = mediaFile.makeDownloadConfigBySetting(
-        this.filenameSetting,
-        this.mode
-      )
-
-      const downloadCallback = this.record_config(config)
-      this.isPassToAria2
-        ? chrome.runtime.sendMessage(ARIA2_ID, config)
-        : chrome.downloads.download(config, downloadCallback)
-    })
-  }
-}
 
 /* eslint-disable no-console */
 function showUpdateMessageInConsole(previous: string) {
