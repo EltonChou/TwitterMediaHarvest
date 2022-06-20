4a459c3a8a19303b062e2a8c1734ff9f3a641979
Elton Chou <plscd748@gmail.com>
2022-06-20 16:19:08 +0800

    collect possible links

--- src/content_script/core/Harvester.ts
+++ src/content_script/core/Harvester.ts
@@ -24,7 +24,7 @@ const parseMagicLink = (article: HTMLElement): string => {
 
   if (!linkEle) {
     const links: string[] = []
-    const anchor_elements = select.all('a[href*="status"]', article)
+    const anchor_elements = select.all('a[href]', article)
     anchor_elements.forEach(v => links.push(v.href))
 
     Sentry.addBreadcrumb({
