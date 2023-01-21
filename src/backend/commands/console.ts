ab9b4eab443cee2f9c3dacda3bccb5187206e2fa
Elton Chou <plscd748@gmail.com>
2023-01-21 16:47:35 +0800

    Refined update console messages.

--- src/backend/commands/console.ts
+++ src/backend/commands/console.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 export function showUpdateMessageInConsole(previous: string) {
   const current = chrome.runtime.getManifest().version
-  console.info('The extension has been updated.')
+  console.group('The extension has been updated. Expand to see the details.')
   console.info('Previous version:', previous)
   console.info('Current version:', current)
+  console.groupEnd()
 }
