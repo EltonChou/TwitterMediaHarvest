8014fb4ddec2d2e3f605e2e63a081310605330cb
Elton Chou <plscd748@gmail.com>
2023-04-29 13:40:20 +0800

    Refine fonts in JP and ZH

--- src/pages/themes.ts
+++ src/pages/themes.ts
@@ -9,8 +9,8 @@ const config: ThemeConfig = {
 const theme = extendTheme({
   config,
   fonts: {
-    heading: '"Homenaje", "Open Sans", sans-serif',
-    body: '"Homenaje", "Raleway", sans-serif',
+    heading: '"Homenaje", "Noto Sans JP", "Noto Sans TC", "Open Sans", sans-serif',
+    body: '"Homenaje", "Noto Sans JP", "Noto Sans TC", sans-serif',
   },
   colors: {
     brand: {
