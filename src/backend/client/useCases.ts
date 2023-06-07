c8118d9264d3c00cbb460a98f3d749e7924f20ed
Elton Chou <plscd748@gmail.com>
2023-06-07 20:51:55 +0800

    Refine

--- src/backend/client/useCases.ts
+++ src/backend/client/useCases.ts
@@ -4,22 +4,27 @@ import { IClientInfoRepository, InfoSyncLock } from './repositories'
 export class ClientInfoUseCase {
   constructor(private infoRepo: IClientInfoRepository) {}
 
+  async initClient(callback?: (clientId: string) => void): Promise<void> {
+    const info = await this.infoRepo.getInfo()
+    if(callback) callback(info.props.uuid)
+  }
+
   async sync(): Promise<void> {
     const lock = new InfoSyncLock(Browser.storage.local)
     if (await lock.isLocked()) return
+    await lock.acquire()
 
     let err: Error = undefined
     const info = await this.infoRepo.getInfo()
     if (info.needSync) {
-      await lock.acquire()
       try {
         await this.infoRepo.updateStats(info.props.csrfToken)
       } catch (error) {
         err = error
-      } finally {
-        await lock.release()
       }
-      if (err) throw err
     }
+
+    await lock.release()
+    if (err) throw err
   }
 }
