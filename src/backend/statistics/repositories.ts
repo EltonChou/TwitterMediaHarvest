f3db05d98ca6a91d4b1f2428408ceca2b3006159
Elton Chou <plscd748@gmail.com>
2023-06-14 18:58:35 +0800

    Housekeeping

--- src/backend/statistics/repositories.ts
+++ src/backend/statistics/repositories.ts
@@ -1,71 +1,13 @@
 import type { Storage } from 'webextension-polyfill'
 
-export enum StatisticsKey {
-  SuccessDownloadCount = 'successDownloadCount',
-  FailedDownloadCount = 'failedDownloadCount',
-  ErrorCount = 'errorCount',
-}
-
-type DownloadStatistic = {
-  [StatisticsKey.SuccessDownloadCount]?: number
-  [StatisticsKey.FailedDownloadCount]?: number
-  [StatisticsKey.ErrorCount]?: number
-}
-
-const defaultStatistic: DownloadStatistic = {
-  [StatisticsKey.ErrorCount]: 0,
-  [StatisticsKey.FailedDownloadCount]: 0,
-  [StatisticsKey.SuccessDownloadCount]: 0,
-}
-
 const defaultV4Stats: V4Statistics = {
   downloadCount: 0,
   trafficUsage: 0,
 }
 
-/**
- * @deprecated
- */
-export interface IStatisticsRepository {
-  getStatisticsCount(key: StatisticsKey): Promise<number>
-  addStatisticsCount(key: StatisticsKey): Promise<void>
-}
-
 export interface IStatisticsRepositoryV4 {
   getStats(): Promise<V4Statistics>
   saveStats(stats: V4Statistics): Promise<void>
-}
-
-/**
- * @deprecated
- */
-export class StatisticsRepository implements IStatisticsRepository {
-  constructor(readonly storageArea: Storage.StorageArea) {}
-
-  async getStatistics(): Promise<DownloadStatistic> {
-    const stats = await this.storageArea.get(defaultStatistic)
-    return stats
-  }
-
-  async getStatisticsCount(key: StatisticsKey): Promise<number> {
-    const downloadStatistic: DownloadStatistic = {}
-    downloadStatistic[key] = 0
-
-    const count = await this.storageArea.get(downloadStatistic)
-    return count[key]
-  }
-
-  async addStatisticsCount(key: StatisticsKey): Promise<void> {
-    const count = await this.getStatisticsCount(key)
-    const downloadCount: DownloadStatistic = {}
-    downloadCount[key] = count + 1
-
-    await this.storageArea.set(downloadCount)
-  }
-
-  async setDefaultStatistics(defaultStats: DownloadStatistic = defaultStatistic): Promise<void> {
-    await this.storageArea.set(defaultStats)
-  }
 }
 
 export class V4StatisticsRepository implements IStatisticsRepositoryV4 {
--- src/backend/statistics/useCases.ts
+++ src/backend/statistics/useCases.ts
@@ -1,51 +1,5 @@
-import { IStatisticsRepository, IStatisticsRepositoryV4, StatisticsKey } from './repositories'
+import { IStatisticsRepositoryV4 } from './repositories'
 import Browser from 'webextension-polyfill'
-
-/**
- * @deprecated The method should not be used
- */
-export interface IStatisticsUseCase {
-  getSuccessDownloadCount(): Promise<number>
-  getFailedDownloadCount(): Promise<number>
-  getErrorCount(): Promise<number>
-  addSuccessDownloadCount(): Promise<void>
-  addFailedDownloadCount(): Promise<void>
-  addErrorCount(): Promise<void>
-}
-
-/**
- * @deprecated Use {@link V4StatsUseCase} instead
- */
-export class StatisticsUseCases implements IStatisticsUseCase {
-  constructor(readonly statisticsRepo: IStatisticsRepository) {}
-
-  async getSuccessDownloadCount(): Promise<number> {
-    const count = await this.statisticsRepo.getStatisticsCount(StatisticsKey.SuccessDownloadCount)
-    return count
-  }
-
-  async getFailedDownloadCount(): Promise<number> {
-    const count = await this.statisticsRepo.getStatisticsCount(StatisticsKey.FailedDownloadCount)
-    return count
-  }
-
-  async getErrorCount(): Promise<number> {
-    const count = await this.statisticsRepo.getStatisticsCount(StatisticsKey.ErrorCount)
-    return count
-  }
-
-  async addSuccessDownloadCount(): Promise<void> {
-    await this.statisticsRepo.addStatisticsCount(StatisticsKey.SuccessDownloadCount)
-  }
-
-  async addFailedDownloadCount(): Promise<void> {
-    await this.statisticsRepo.addStatisticsCount(StatisticsKey.FailedDownloadCount)
-  }
-
-  async addErrorCount(): Promise<void> {
-    await this.statisticsRepo.addStatisticsCount(StatisticsKey.ErrorCount)
-  }
-}
 
 export class V4StatsUseCase {
   constructor(readonly statisticsRepo: IStatisticsRepositoryV4) {}
@@ -76,7 +30,7 @@ export class V4StatsUseCase {
       (stats, curr) => {
         if (curr.byExtensionId === Browser.runtime.id) {
           stats.downloadCount += 1
-          stats.trafficUsage += curr.fileSize < 0 ? 0 : curr.fileSize
+          stats.trafficUsage += Math.max(curr.fileSize, 0)
         }
         return stats
       },
