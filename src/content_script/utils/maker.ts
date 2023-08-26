b687e36aa08c92fb2abf227960b6f168e7841604
Elton Chou <plscd748@gmail.com>
2023-08-27 05:56:03 +0800

    Added archived button style.

--- src/content_script/utils/maker.ts
+++ src/content_script/utils/maker.ts
@@ -1,11 +1,11 @@
-import { FailedToParseTweetInfoNotifyUseCase } from '@backend/notifications/notifyUseCases'
-import { Action, exchangeInternal, HarvestExchange } from '@libs/browser'
-import { toError } from 'fp-ts/lib/Either'
-import { pipe } from 'fp-ts/lib/function'
-import { type IOEither } from 'fp-ts/lib/IOEither'
-import * as TE from 'fp-ts/lib/TaskEither'
-import { ButtonStatus } from '../../enums'
-import { captureExceptionIO } from './helper'
+import { ButtonStatus } from '../../enums';
+import { captureExceptionIO } from './helper';
+import { FailedToParseTweetInfoNotifyUseCase } from '@backend/notifications/notifyUseCases';
+import { Action, HarvestExchange, HarvestResponse, exchangeInternal } from '@libs/browser';
+import { toError } from 'fp-ts/lib/Either';
+import { type IOEither } from 'fp-ts/lib/IOEither';
+import * as TE from 'fp-ts/lib/TaskEither';
+import { pipe } from 'fp-ts/lib/function';
 
 /**
  * Create HTMLElement from html string.
@@ -20,14 +20,22 @@ export const createElementFromHTML = (htmlString: string): HTMLElement => {
 
 const setButtonStatus = (status: ButtonStatus) => (button: HTMLElement) => {
   if (!button) return button
-  button.classList.remove('downloading', 'success', 'error')
+  button.classList.remove(
+    ButtonStatus.Downloading,
+    ButtonStatus.Success,
+    ButtonStatus.Error,
+    ButtonStatus.Downloaded
+  )
   button.classList.add(status)
   return button
 }
 
-const isButtonDownloading = (button: HTMLElement) => button.classList.contains('downloading')
+const isButtonDownloading = (button: HTMLElement) =>
+  button.classList.contains('downloading')
 
-const sendExchange = (exchange: HarvestExchange<Action.Download>) =>
+const sendExchange = <A extends Action>(
+  exchange: HarvestExchange<A>
+): TE.TaskEither<Error, HarvestResponse<A>> =>
   TE.tryCatch(async () => exchangeInternal(exchange), toError)
 
 const notifyInfoParserError = TE.tryCatch(async () => {
@@ -47,13 +55,35 @@ export const makeButtonListener =
       pipe(
         TE.Do,
         TE.bind('data', () => pipe(infoProvider, TE.fromIOEither)),
-        TE.tapError(e => pipe(e, captureExceptionIO, TE.fromIO, () => notifyInfoParserError)),
-        TE.bind('response', exchange => sendExchange({ action: Action.Download, data: exchange.data })),
+        TE.tapError(e =>
+          pipe(e, captureExceptionIO, TE.fromIO, () => notifyInfoParserError)
+        ),
+        TE.bind('response', exchange =>
+          sendExchange({ action: Action.Download, data: exchange.data })
+        ),
         TE.match(
           () => ButtonStatus.Error,
-          ({ response }) => (response.status === 'success' ? ButtonStatus.Success : ButtonStatus.Error)
+          ({ response }) =>
+            response.status === 'success' ? ButtonStatus.Success : ButtonStatus.Error
         )
       )().then(status => setButtonStatus(status)(button))
+    })
+
+    const checkTweetHasDownloaded = pipe(
+      infoProvider,
+      TE.fromIOEither,
+      TE.chain(info =>
+        sendExchange({ action: Action.CheckDownloadHistory, data: info.tweetId })
+      ),
+      TE.map(resp => (resp.status === 'error' ? false : resp.data)),
+      TE.match(
+        () => false,
+        r => r
+      )
+    )
+
+    checkTweetHasDownloaded().then(r => {
+      r ? setButtonStatus(ButtonStatus.Downloaded)(button) : {}
     })
 
     return button
