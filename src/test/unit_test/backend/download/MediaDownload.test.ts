ebaae09837a9b29f6bf620793d5db2acdd57b200
Elton Chou <plscd748@gmail.com>
2023-09-15 19:48:05 +0800

    Fix test case

--- src/test/unit_test/backend/download/MediaDownload.test.ts
+++ src/test/unit_test/backend/download/MediaDownload.test.ts
@@ -1,8 +1,32 @@
 import MediaDownloader from '@backend/downloads/MediaDownloader'
+import { DownloadRecord } from '@backend/downloads/models'
+import type { IDownloadRecordsRepository } from '@backend/downloads/repositories'
 import type { DownloadSettings, FeatureSettings, V4FilenameSettings } from '@schema'
 
 const mockDownload = jest.fn()
 const mockSendMessage = jest.fn(() => new Promise(resolve => resolve('123')))
+
+class MockRecordRepository implements IDownloadRecordsRepository {
+  async getById(downloadItemId: number): Promise<DownloadRecord> {
+    return new DownloadRecord(downloadItemId, {
+      tweetInfo: {
+        tweetId: '123',
+        screenName: 'user',
+      },
+      downloadConfig: {
+        url: 'https://www.google.com',
+      },
+    })
+  }
+
+  async save(downloadRecord: DownloadRecord): Promise<void> {
+    return
+  }
+
+  async removeById(downloadItemId: number): Promise<void> {
+    return
+  }
+}
 
 jest.mock('webextension-polyfill', () => {
   return {
@@ -72,7 +96,8 @@ describe('MediaDownloader Unit Test', () => {
     const downloader = new MediaDownloader(
       filenameSettings,
       downloadSettings,
-      featureSettings
+      featureSettings,
+      new MockRecordRepository()
     )
     await downloader.downloadMediasByMediaCatalog(tweetDetail)(mediaCatalog)
     expect(mockDownload).toBeCalled()
@@ -82,26 +107,37 @@ describe('MediaDownloader Unit Test', () => {
     const downloader = new MediaDownloader(
       filenameSettings,
       { ...downloadSettings, enableAria2: true },
-      featureSettings
+      featureSettings,
+      new MockRecordRepository()
     )
     await downloader.downloadMediasByMediaCatalog(tweetDetail)(mediaCatalog)
     expect(mockSendMessage).toBeCalled()
   })
 
   it('can download medias from media catalog includes thumbnail', async () => {
-    const downloader = new MediaDownloader(filenameSettings, downloadSettings, {
-      ...featureSettings,
-      includeVideoThumbnail: true,
-    })
+    const downloader = new MediaDownloader(
+      filenameSettings,
+      downloadSettings,
+      {
+        ...featureSettings,
+        includeVideoThumbnail: true,
+      },
+      new MockRecordRepository()
+    )
     await downloader.downloadMediasByMediaCatalog(tweetDetail)(mediaCatalog)
     expect(mockDownload).toBeCalledTimes(4)
   })
 
   it('can download medias from media catalog excludes thumbnail', async () => {
-    const downloader = new MediaDownloader(filenameSettings, downloadSettings, {
-      ...featureSettings,
-      includeVideoThumbnail: false,
-    })
+    const downloader = new MediaDownloader(
+      filenameSettings,
+      downloadSettings,
+      {
+        ...featureSettings,
+        includeVideoThumbnail: false,
+      },
+      new MockRecordRepository()
+    )
     await downloader.downloadMediasByMediaCatalog(tweetDetail)(mediaCatalog)
     expect(mockDownload).toBeCalledTimes(3)
   })
