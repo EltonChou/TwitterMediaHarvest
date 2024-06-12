e790f0d3b450c5cf7c68ac4fe33b6071f6fad168
Elton Chou <plscd748@gmail.com>
2024-06-12 17:20:12 +0800

    refactor: Refactor storage proxy.

--- src/libs/proxy.ts
+++ src/libs/proxy.ts
@@ -2,8 +2,6 @@ import type {
   LocalStorageSchema as LocalSchema,
   SyncStorageSchema as SyncSchema,
 } from '@schema'
-import type { Storage } from 'webextension-polyfill'
-import Browser from 'webextension-polyfill'
 
 export type SchemaKey<T> = string extends T
   ? never
@@ -30,51 +28,7 @@ export interface IStorageProxy<SchemaT extends Record<string, any>> {
   removeItem<Key extends SchemaKey<SchemaT>>(keys: Key | Key[]): Promise<void>
 }
 
-// eslint-disable-next-line max-len, prettier/prettier
-abstract class ExtensionStorageProxy<T extends LocalSchema | SyncSchema>
-  implements IStorageProxy<T>
-{
-  constructor(readonly storageArea: Storage.StorageArea) {}
-
-  async setItem(item: Partial<T>): Promise<void> {
-    await this.storageArea.set(item)
-  }
-
-  async removeItem<Key extends SchemaKey<T>>(keys: Key | Key[]): Promise<void> {
-    await this.storageArea.remove(
-      Array.isArray(keys) ? [...keys].map(v => String(v)) : String(keys)
-    )
-  }
-
-  async getItemByKey<Key extends SchemaKey<T>>(key: Key): Promise<Pick<T, Key>> {
-    const strKey = String(key)
-    const record = await this.storageArea.get(strKey)
-    return Object.keys(record).includes(strKey)
-      ? (record as Pick<T, SchemaKey<T>>)
-      : undefined
-  }
-
-  async getItemByDefaults<Defaults extends Partial<T>>(
-    defaults: Defaults
-  ): Promise<Defaults> {
-    const record = await this.storageArea.get(defaults)
-    return { ...defaults, ...record }
-  }
-}
-
-export class LocalExtensionStorageProxy extends ExtensionStorageProxy<LocalSchema> {
-  constructor(storageArea?: Storage.StorageArea) {
-    super(storageArea || Browser.storage.local)
-  }
-}
-
-export class SyncExtensionStorageProxy extends ExtensionStorageProxy<SyncSchema> {
-  constructor(storageArea?: Storage.StorageArea) {
-    super(storageArea || Browser.storage.sync)
-  }
-}
-
-export class SimpleObjectStorageProxy<T extends LocalSchema | SyncSchema>
+export class SimpleObjectStorageProxy<T extends Partial<LocalSchema | SyncSchema>>
   implements IStorageProxy<T>
 {
   constructor(protected storage: Record<string, any> = {}) {}
