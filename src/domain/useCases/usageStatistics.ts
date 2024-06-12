20c259976f11a1d39cd7081420dbc8f3fc5b039a
Elton Chou <plscd748@gmail.com>
2024-06-12 19:34:36 +0800

    refactor: Improve usage statistics use case.

--- src/domain/useCases/usageStatistics.ts
+++ src/domain/useCases/usageStatistics.ts
@@ -1,5 +1,7 @@
 import type { V4Statistics } from '#schema'
 
 export interface IUsageStatisticsUseCase {
+  get(): Promise<V4Statistics>
   increase(statsDelta: V4Statistics): Promise<void>
+  syncWithDownloadHistory(): Promise<void>
 }
--- src/infra/useCases/usageStatistics.ts
+++ src/infra/useCases/usageStatistics.ts
@@ -1,19 +1,49 @@
 import type { IUsageStatisticsUseCase } from '#domain/useCases/usageStatistics'
 import type { IStorageProxy } from '@libs/proxy'
 import type { V4Statistics } from '@schema'
+import Browser from 'webextension-polyfill'
 
 const defaultV4Stats: V4Statistics = Object.freeze({
   downloadCount: 0,
   trafficUsage: 0,
 })
 
-export class V4StatisticsRepository implements IUsageStatisticsUseCase {
+export class UsageStatisticsUseCase implements IUsageStatisticsUseCase {
   constructor(readonly storage: IStorageProxy<V4Statistics>) {}
+
+  async get(): Promise<V4Statistics> {
+    return this.storage.getItemByDefaults(defaultV4Stats)
+  }
 
   async increase(statsDelta: V4Statistics): Promise<void> {
     const stats = await this.storage.getItemByDefaults(defaultV4Stats)
-    stats.downloadCount += statsDelta.downloadCount
-    stats.trafficUsage += statsDelta.trafficUsage
+    stats.downloadCount += Math.max(statsDelta.downloadCount, 0)
+    stats.trafficUsage += Math.max(statsDelta.trafficUsage, 0)
     await this.storage.setItem(stats)
+  }
+
+  async syncWithDownloadHistory(): Promise<void> {
+    const pastItems = await Browser.downloads.search({ limit: 0 })
+    const stats = await this.get()
+    const syncStats = pastItems.reduce(
+      (stats, curr) => {
+        if (curr.byExtensionId === Browser.runtime.id) {
+          stats.downloadCount += 1
+          stats.trafficUsage += Math.max(curr.fileSize, 0)
+        }
+        return stats
+      },
+      {
+        downloadCount: 0,
+        trafficUsage: 0,
+      } as V4Statistics
+    )
+
+    if (
+      syncStats.downloadCount >= stats.downloadCount &&
+      syncStats.trafficUsage >= stats.trafficUsage
+    ) {
+      await this.storage.setItem(syncStats)
+    }
   }
 }
