e910a18c5306c910172a26d46a74a969f66a4efa
Elton Chou <plscd748@gmail.com>
2024-06-22 02:05:07 +0800

    refactor (helper): Add download config helpers.

---
+++ src/helpers/downloadConfig.ts
@@ -0,0 +1,42 @@
+import type { Downloads } from 'webextension-polyfill'
+
+/**
+ * Create browser download config object.
+ *
+ * @param url
+ * @param fileName
+ */
+export const makeBrowserDownloadConfig = (
+  url: string,
+  fileName: string,
+  askPath: boolean
+): Downloads.DownloadOptionsType => {
+  const options: Downloads.DownloadOptionsType = {
+    url: url,
+    filename: fileName,
+    conflictAction: 'overwrite',
+  }
+  return process.env.TARGET === 'firefox' ? { ...options, saveAs: askPath } : options
+}
+
+/**
+ * Create aria2 download config object.
+ *
+ * @param url
+ * @param fileName
+ * @param referrer
+ * @param options aria2 options
+ */
+export const makeAria2DownloadConfig = (
+  url: string,
+  fileName: string,
+  referrer: string,
+  options: object = {}
+): Aria2DownloadOption => {
+  return {
+    url: url,
+    filename: fileName,
+    referrer: referrer,
+    options: options,
+  }
+}
