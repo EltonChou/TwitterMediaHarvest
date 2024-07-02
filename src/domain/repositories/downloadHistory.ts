7b5ab99920ed2a5cedddaed958d9adad3e1a1fc0
Elton Chou <plscd748@gmail.com>
2024-07-02 20:08:09 +0800

    refactor: Add downloadHistory repository interface.

---
+++ src/domain/repositories/downloadHistory.ts
@@ -0,0 +1,7 @@
+import type { DownloadHistory } from '#domain/entities/downloadHistory'
+
+export interface IDownloadHistoryRepository {
+  count(): Promise<number>
+  save(item: DownloadHistory): Promise<void>
+  getByTweetId(tweetId: string): Promise<DownloadHistory | undefined>
+}
