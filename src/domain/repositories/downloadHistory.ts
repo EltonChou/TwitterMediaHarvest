b7b3593354c626f1e43a6d6136962c52b09c099a
Elton Chou <plscd748@gmail.com>
2024-07-04 16:04:48 +0800

    refactor: Add `removeByTweetId` method in download history repository interface.

--- src/domain/repositories/downloadHistory.ts
+++ src/domain/repositories/downloadHistory.ts
@@ -4,4 +4,5 @@ export interface IDownloadHistoryRepository {
   count(): Promise<number>
   save(item: DownloadHistory): Promise<void>
   getByTweetId(tweetId: string): Promise<DownloadHistory | undefined>
+  removeByTweetId(tweetId: string): Promise<void>
 }
