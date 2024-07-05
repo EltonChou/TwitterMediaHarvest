379f594da39e4d5445bae6aaf553db3eec37763e
Elton Chou <plscd748@gmail.com>
2024-07-05 22:29:04 +0800

    chore: Update configuration.

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  verbose: true,
+  transform: {
+    '\\.[jt]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  setupFiles: ['fake-indexeddb/auto'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '#libs/(.*)$': '<rootDir>/src/libs/$1',
+    '#backend/(.*)$': '<rootDir>/src/backend/$1',
+    '#domain/(.*)$': '<rootDir>/src/domain/$1',
+    '#helpers/(.*)$': '<rootDir>/src/helpers/$1',
+    '#enums/(.*)$': '<rootDir>/src/enums/$1',
+    '#infra/(.*)$': '<rootDir>/src/infra/$1',
+    '#constants': '<rootDir>/src/constants',
+    '#utils/(.*)$': '<rootDir>/src/utils/$1',
+    '#eventHandlers/(.*)$': '<rootDir>/src/eventHandlers/$1',
+    '#mocks/(.*)$': '<rootDir>/src/mocks/$1',
+  },
+  coveragePathIgnorePatterns: [],
+}
+
+export default jestConfig
