616d90e6e5dba893050b2febbc1dc1b540e68719
Elton Chou <plscd748@gmail.com>
2024-07-18 02:49:54 +0800

    refactor: Add searching download history domain use case.

--- src/domain/useCases/searchDownloadHistory.ts
+++ src/domain/useCases/searchDownloadHistory.ts
@@ -1,15 +1,30 @@
 import { DownloadHistory } from '#domain/entities/downloadHistory'
+import MediaType from '#enums/mediaType'
 import type { AsyncUseCase } from './base'
 
-type QueryResult = {
-  items: DownloadHistory[]
-  matchedCount: number
-}
+type QueryResult =
+  | {
+      items: DownloadHistory[]
+      matchedCount: number
+      error: undefined
+    }
+  | {
+      items: []
+      matchedCount: -1
+      error: Error
+    }
+
+type Filter = (item: {
+  screenName?: string
+  displayName?: string
+  mediaType?: MediaType
+}) => boolean
 
 type Query = {
-  limit?: number
-  skip?: number
-  predicates: (item: DownloadHistory) => boolean
+  limit: number
+  skip: number
+  filters: Filter[]
+  hashtags: string[]
 }
 
 export type SearchDownloadHistoryUseCase = AsyncUseCase<Query, QueryResult>
