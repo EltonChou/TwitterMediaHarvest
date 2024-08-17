85df5637dec4228a83f5bac981e91ea15621bf7d
Elton Chou <plscd748@gmail.com>
2024-08-18 00:19:05 +0800

    refactor: Add generating function for test usage.

---
+++ src/utils/test/downloadHistoryItem.ts
@@ -0,0 +1,19 @@
+import MediaType from '#enums/mediaType'
+import type { DownloadHistoryItem } from '#libs/idb/download/schema'
+import { faker } from '@faker-js/faker'
+
+export const generateDownloadHistoryItem = (): DownloadHistoryItem => ({
+  displayName: faker.internet.displayName(),
+  screenName: faker.internet.userName(),
+  downloadTime: faker.date.recent(),
+  hashtags: new Set(['a', 'b', 'c']),
+  mediaType: faker.helpers.arrayElement([
+    MediaType.Mixed,
+    MediaType.Image,
+    MediaType.Video,
+  ]),
+  thumbnail: faker.image.url(),
+  tweetId: '1',
+  tweetTime: faker.date.recent(),
+  userId: faker.string.numeric(),
+})
---
+++ src/utils/test/v5ProtableHistoryItem.ts
@@ -0,0 +1,20 @@
+import { V5PortableDownloadHistoryItem } from '#domain/valueObjects/portableDownloadHistoryItem'
+import MediaType from '#enums/mediaType'
+import { faker } from '@faker-js/faker'
+
+export const generatePortableDownloadHistoryItem = () =>
+  new V5PortableDownloadHistoryItem({
+    displayName: faker.internet.displayName(),
+    screenName: faker.internet.userName(),
+    downloadTime: faker.date.recent(),
+    hashtags: faker.helpers.arrayElements(['a', 'b', 'c']),
+    mediaType: faker.helpers.arrayElement([
+      MediaType.Mixed,
+      MediaType.Image,
+      MediaType.Video,
+    ]),
+    thumbnail: faker.image.url(),
+    tweetId: '1',
+    tweetTime: faker.date.recent(),
+    userId: faker.string.numeric(),
+  })
