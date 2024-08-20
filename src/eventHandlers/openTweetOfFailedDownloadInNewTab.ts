928d7ec1bd317e01b8b803b9658a3fc4e0f47ec4
Elton Chou <plscd748@gmail.com>
2024-08-21 01:50:17 +0800

    refactor: Implement handler to open tweet of failed download in new tab.

---
+++ src/eventHandlers/openTweetOfFailedDownloadInNewTab.ts
@@ -0,0 +1,25 @@
+import type { DomainEventHandler } from '#domain/eventPublisher'
+import type { Factory } from '#domain/factories/base'
+import type { IDownloadRecordRepository } from '#domain/repositories/downloadRecord'
+import type { DownloadRecord } from '#domain/valueObjects/downloadRecord'
+import { tabs } from 'webextension-polyfill'
+
+const tweetUrl = (tweetId: string) => `https://x.com/i/web/status/${tweetId}`
+
+export const openTweetOfFailedDownloadInNewTab =
+  (
+    downloadRecordRepo: IDownloadRecordRepository
+  ): DomainEventHandler<DownloadFailedNotificationEvent> =>
+  async event => {
+    const { value: record, error } = await downloadRecordRepo.getById(event.downloadId)
+
+    // TODO: Log error
+    if (error) return
+
+    await tabs.create({
+      url: tweetUrl(recordToTweetId(record)),
+    })
+  }
+
+const recordToTweetId: Factory<DownloadRecord, string> = record =>
+  record.mapBy(props => props.tweetInfo.mapBy(props => props.tweetId))
---
+++ src/eventHandlers/openTweetOffailedDownloadInNewTab.test.ts
@@ -0,0 +1,23 @@
+import DownloadFailedNotificationViewButtonClicked from '#domain/events/DownloadFailedNotificationViewButtonClicked'
+import { MockEventPublisher } from '#mocks/eventPublisher'
+import { MockDownloadRecordRepo } from '#mocks/repositories/downloadRecord'
+import { toSuccessResult } from '#utils/result'
+import { generateDownloadRecord } from '#utils/test/downloadRecord'
+import { openTweetOfFailedDownloadInNewTab } from './openTweetOfFailedDownloadInNewTab'
+import { tabs } from 'webextension-polyfill'
+
+describe('unit test for handler to open tweet of failed download in new tab', () => {
+  const recordRepo = new MockDownloadRecordRepo()
+  const publisher = new MockEventPublisher()
+
+  it('can handle download failed notification event', async () => {
+    jest
+      .spyOn(recordRepo, 'getById')
+      .mockResolvedValue(toSuccessResult(generateDownloadRecord()))
+    const mockCreateTab = jest.spyOn(tabs, 'create')
+    const handler = openTweetOfFailedDownloadInNewTab(recordRepo)
+    await handler(new DownloadFailedNotificationViewButtonClicked(10), publisher)
+
+    expect(mockCreateTab).toHaveBeenCalled()
+  })
+})
