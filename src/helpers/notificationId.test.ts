75fc5ab5a8ba9f61ac69d2199313bc8a7b8b5e92
Elton Chou <plscd748@gmail.com>
2024-08-22 19:29:20 +0800

    refactor: Add notification id check and extractor.

--- src/helpers/notificationId.test.ts
+++ src/helpers/notificationId.test.ts
@@ -1,12 +1,40 @@
 import {
+  extractDownloadId,
+  extractTweetId,
+  isDownloadId,
+  isTweetFetchId,
   makeDownloadFailedNotificationId,
   makeTweetFetchErrorNotificationId,
 } from './notificationId'
 
-test('tweet fetch error notification id maker', () => {
-  expect(makeTweetFetchErrorNotificationId('123')).toBe('tweet_123')
-})
+describe('unit test for notification id helper', () => {
+  it('can make tweet fetch error notification id', () => {
+    expect(makeTweetFetchErrorNotificationId('123')).toBe('tweet_123')
+  })
 
-test('download failed notification id maker', () => {
-  expect(makeDownloadFailedNotificationId(1)).toBe('download_1')
+  it('can download failed notification id', () => {
+    expect(makeDownloadFailedNotificationId(1)).toBe('download_1')
+  })
+
+  it('validate download id', () => {
+    const downloadId = 'download_123'
+    expect(isDownloadId(downloadId)).toBeTruthy()
+    expect(isDownloadId('some_123')).toBeFalsy()
+  })
+
+  it('validate tweet id', () => {
+    const tweetId = 'tweet_1145141919810'
+    expect(isTweetFetchId(tweetId)).toBeTruthy()
+    expect(isTweetFetchId('some_123')).toBeFalsy()
+  })
+
+  it('can extract download id', () => {
+    const downloadId = 'download_123'
+    expect(extractDownloadId(downloadId)).toBe(123)
+  })
+
+  it('extract tweet id', () => {
+    const tweetId = 'tweet_1145141919810'
+    expect(extractTweetId(tweetId)).toBe('1145141919810')
+  })
 })
--- src/helpers/notificationId.ts
+++ src/helpers/notificationId.ts
@@ -1,3 +1,29 @@
-export const makeTweetFetchErrorNotificationId = (tweetId: string) => `tweet_${tweetId}`
-export const makeDownloadFailedNotificationId = (downloadItemId: number) =>
-  `download_${downloadItemId}`
+type TweetNotificationId = `tweet_${string}`
+type DownloadNotificationId = `download_${number}`
+
+export const makeTweetFetchErrorNotificationId = (tweetId: string): TweetNotificationId =>
+  `tweet_${tweetId}`
+export const makeDownloadFailedNotificationId = (
+  downloadItemId: number
+): DownloadNotificationId => `download_${downloadItemId}`
+
+const tweetIdPattern = /^tweet_(\d+)$/
+const downloadIdPattern = /^download_(\d+)$/
+
+export const isTweetFetchId = (id: string): id is TweetNotificationId =>
+  Boolean(id.match(tweetIdPattern))
+
+export const isDownloadId = (id: string): id is DownloadNotificationId =>
+  Boolean(id.match(downloadIdPattern))
+
+export const extractDownloadId = (id: DownloadNotificationId): number => {
+  const result = id.match(downloadIdPattern)
+  if (!result) throw new Error(`${id} is not a download notification id.`)
+  return Number(result[1])
+}
+
+export const extractTweetId = (id: TweetNotificationId): string => {
+  const result = id.match(tweetIdPattern)
+  if (!result) throw new Error(`${id} is not a tweet notification id.`)
+  return result[1]
+}
