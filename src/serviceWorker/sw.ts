8fd1f933da4e318d63fd68dac9446cd55cb6bb46
Elton Chou <plscd748@gmail.com>
2024-08-25 03:13:40 +0800

    refactor: Implement notification event listeners.

--- src/serviceWorker/sw.ts
+++ src/serviceWorker/sw.ts
@@ -1,6 +1,24 @@
 import DownloadCompleted from '#domain/events/DownloadCompleted'
+import DownloadFailedNotificationClicked from '#domain/events/DownloadFailedNotificationClicked'
+import DownloadFailedNotificationClosed from '#domain/events/DownloadFailedNotificationClosed'
+import DownloadFailedNotificationRetryButtonClicked from '#domain/events/DownloadFailedNotificationRetryButtonClicked'
+import DownloadFailedNotificationViewButtonClicked from '#domain/events/DownloadFailedNotificationViewButtonClicked'
 import RuntimeInstalled from '#domain/events/RuntimeInstalled'
 import RuntimeUpdated from '#domain/events/RuntimeUpdated'
+import TweetFetchErrorNotificationClicked from '#domain/events/TweetFetchErrorNotificationClicked'
+import TweetFetchErrorNotificationClosed from '#domain/events/TweetFetchErrorNotificationClosed'
+import TweetFetchErrorNotificationViewButtonClicked from '#domain/events/TweetFetchErrorNotificationViewButtonClicked'
+import { UnknownNotificationButtonClicked } from '#domain/events/UnknownNotificationButtonClicked'
+import {
+  GeneralTweetFetchErrorNotificationButton,
+  MediaDownloadNotificationErrorButton,
+} from '#helpers/notificationConfig'
+import {
+  extractDownloadId,
+  extractTweetId,
+  isDownloadId,
+  isTweetFetchId,
+} from '#helpers/notificationId'
 import { getEventPublisher } from '#infra/eventPublisher'
 import Browser from 'webextension-polyfill'
 
@@ -38,14 +56,75 @@ Browser.downloads.onChanged.addListener(async downloadDelta => {
   }
 })
 
-Browser.notifications.onClosed.addListener(notifficationId => {
-  //TODO: Check notification type.
+Browser.notifications.onClosed.addListener(notificationId => {
+  if (isTweetFetchId(notificationId)) {
+    const tweetId = extractTweetId(notificationId)
+    eventPublisher.publish(new TweetFetchErrorNotificationClosed(tweetId))
+    return
+  }
+
+  if (isDownloadId(notificationId)) {
+    const downloadId = extractDownloadId(notificationId)
+    eventPublisher.publish(new DownloadFailedNotificationClosed(downloadId))
+    return
+  }
 })
 
-Browser.notifications.onClicked.addListener(notifficationId => {
-  //TODO: Check notification type.
+Browser.notifications.onClicked.addListener(notificationId => {
+  if (isTweetFetchId(notificationId)) {
+    const tweetId = extractTweetId(notificationId)
+    eventPublisher.publish(new TweetFetchErrorNotificationClicked(tweetId))
+    return
+  }
+
+  if (isDownloadId(notificationId)) {
+    const downloadId = extractDownloadId(notificationId)
+    eventPublisher.publish(new DownloadFailedNotificationClicked(downloadId))
+    return
+  }
 })
 
-Browser.notifications.onButtonClicked.addListener((notifficationId, buttonIndex) => {
-  //TODO: Check notification type.
+Browser.notifications.onButtonClicked.addListener((notificationId, buttonIndex) => {
+  if (isTweetFetchId(notificationId)) {
+    const tweetId = extractTweetId(notificationId)
+
+    let event: IDomainEvent
+    switch (buttonIndex) {
+      case GeneralTweetFetchErrorNotificationButton.ViewTweet:
+        event = new TweetFetchErrorNotificationViewButtonClicked(tweetId)
+        break
+
+      default:
+        event = new UnknownNotificationButtonClicked(buttonIndex)
+        break
+    }
+
+    eventPublisher.publish(event)
+
+    return
+  }
+
+  if (isDownloadId(notificationId)) {
+    const downloadId = extractDownloadId(notificationId)
+
+    let event: IDomainEvent
+    switch (buttonIndex) {
+      case MediaDownloadNotificationErrorButton.RetryDownload:
+        event = new DownloadFailedNotificationRetryButtonClicked(downloadId)
+        break
+
+      case MediaDownloadNotificationErrorButton.ViewTweet:
+        event = new DownloadFailedNotificationViewButtonClicked(downloadId)
+        break
+
+      default:
+        event = new UnknownNotificationButtonClicked(buttonIndex)
+        break
+    }
+
+    eventPublisher.publish(event)
+    return
+  }
+
+  return
 })
