cade896c8f315ba3e6cf357a1c2ba7bd2c6b0268
Elton Chou <plscd748@gmail.com>
2024-08-25 18:04:31 +0800

    refactor: Change util name.

--- src/utils/runtime.ts
+++ src/utils/runtime.ts
@@ -4,3 +4,5 @@ export const getVersion = () => Browser.runtime.getManifest().version
 
 export const getFullVersion = () =>
   Browser.runtime.getManifest().version_name ?? getVersion()
+
+export const getRuntimeId = () => Browser.runtime.id
