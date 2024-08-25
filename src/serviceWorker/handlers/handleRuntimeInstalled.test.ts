193757bcf44bf8afbd1dc97a5c8dc787cc23c48c
Elton Chou <plscd748@gmail.com>
2024-08-25 19:08:19 +0800

    refactor: Implement service worker handler to handle runtime installed.

---
+++ src/serviceWorker/handlers/handleRuntimeInstalled.test.ts
@@ -0,0 +1,51 @@
+import { getEventPublisher } from '#infra/eventPublisher'
+import handleRuntimeInstalled from './handleRuntimeInstalled'
+import Browser from 'webextension-polyfill'
+
+describe('integration test for handler to handle runtime installed event ', () => {
+  const eventPublisher = getEventPublisher()
+  const mockHandlerRuntimeInstalled = jest.fn()
+  const mockHandlerRuntimeUpdated = jest.fn()
+
+  jest.spyOn(Browser.runtime, 'getManifest').mockReturnValue({
+    manifest_version: 3,
+    name: 'mh-test',
+    version: '5.0.0',
+    version_name: '5.0.0 (test)',
+  })
+
+  beforeAll(() => {
+    eventPublisher
+      .register('runtime:status:installed', mockHandlerRuntimeInstalled)
+      .register('runtime:status:updated', mockHandlerRuntimeUpdated)
+  })
+
+  afterEach(() => {
+    mockHandlerRuntimeInstalled.mockReset()
+    mockHandlerRuntimeUpdated.mockReset()
+  })
+
+  afterAll(() => eventPublisher.clearAllHandlers())
+
+  it('can emit runtime installed event', async () => {
+    const handler = handleRuntimeInstalled(eventPublisher)
+
+    await handler({ reason: 'install', temporary: false })
+    expect(mockHandlerRuntimeInstalled).toHaveBeenCalled()
+  })
+
+  it('can emit runtime updated event', async () => {
+    const handler = handleRuntimeInstalled(eventPublisher)
+
+    await handler({ reason: 'update', temporary: false, previousVersion: '4.0.0' })
+    expect(mockHandlerRuntimeUpdated).toHaveBeenCalled()
+  })
+
+  it('can ignore browser updated event', async () => {
+    const handler = handleRuntimeInstalled(eventPublisher)
+
+    await handler({ reason: 'browser_update', temporary: false })
+    expect(mockHandlerRuntimeInstalled).not.toHaveBeenCalled()
+    expect(mockHandlerRuntimeUpdated).not.toHaveBeenCalled()
+  })
+})
---
+++ src/serviceWorker/handlers/handleRuntimeInstalled.ts
@@ -0,0 +1,28 @@
+import type { DomainEventPublisher } from '#domain/eventPublisher'
+import RuntimeInstalled from '#domain/events/RuntimeInstalled'
+import RuntimeUpdated from '#domain/events/RuntimeUpdated'
+import { getVersion } from '#utils/runtime'
+import type { Runtime } from 'webextension-polyfill'
+
+const handleRuntimeInstalled =
+  (publisher: DomainEventPublisher) =>
+  async (details: Runtime.OnInstalledDetailsType) => {
+    if (details.reason === 'browser_update') return
+
+    const currentVersion = getVersion()
+
+    if (details.reason === 'install') {
+      await publisher.publish(new RuntimeInstalled(currentVersion))
+    }
+
+    if (details.reason === 'update') {
+      await publisher.publish(
+        new RuntimeUpdated({
+          current: currentVersion,
+          previous: details?.previousVersion ?? currentVersion,
+        })
+      )
+    }
+  }
+
+export default handleRuntimeInstalled
