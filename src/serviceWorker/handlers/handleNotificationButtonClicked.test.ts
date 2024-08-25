796a05523efcfd5f3b8bd208373586a4adb70925
Elton Chou <plscd748@gmail.com>
2024-08-25 20:19:12 +0800

    refactor: Implement service worker handler to handle notification button clicked event.

---
+++ src/serviceWorker/handlers/handleNotificationButtonClicked.test.ts
@@ -0,0 +1,61 @@
+import { GeneralTweetFetchErrorNotificationButton } from '#helpers/notificationConfig'
+import {
+  makeDownloadFailedNotificationId,
+  makeTweetFetchErrorNotificationId,
+} from '#helpers/notificationId'
+import { getEventPublisher } from '#infra/eventPublisher'
+import handleNotificationButtonClicked from './handleNotificationButtonClicked'
+
+describe('integration test for handler to handle notification clicked event ', () => {
+  const eventPublisher = getEventPublisher()
+  const mockHandleTweetNotificationButtonClicked = jest.fn()
+  const mockHandleDownloadFailedNotificationButtonClicked = jest.fn()
+
+  beforeAll(() => {
+    eventPublisher
+      .register(
+        'notification:tweetFetchError:viewButton:clicked',
+        mockHandleTweetNotificationButtonClicked
+      )
+      .register(
+        'notification:downloadFailed:viewButton:clicked',
+        mockHandleDownloadFailedNotificationButtonClicked
+      )
+      .register(
+        'notification:downloadFailed:retryButton:clicked',
+        mockHandleDownloadFailedNotificationButtonClicked
+      )
+  })
+
+  afterEach(() => {
+    mockHandleDownloadFailedNotificationButtonClicked.mockReset()
+    mockHandleTweetNotificationButtonClicked.mockReset()
+  })
+
+  afterAll(() => eventPublisher.clearAllHandlers())
+
+  describe('tweet fetching failed notifiactoin case', () => {
+    it('can emit notification:tweetFetchError:viewButton:clicked event', async () => {
+      const handler = handleNotificationButtonClicked(eventPublisher)
+      await handler(makeTweetFetchErrorNotificationId('123'), 0)
+
+      expect(mockHandleTweetNotificationButtonClicked).toHaveBeenCalled()
+    })
+  })
+
+  describe('download failed notifiactoin case', () => {
+    it('can emit notification:downloadFailed:viewButton:clicked event', async () => {
+      const handler = handleNotificationButtonClicked(eventPublisher)
+      await handler(makeDownloadFailedNotificationId(100), 0)
+
+      expect(mockHandleDownloadFailedNotificationButtonClicked).toHaveBeenCalled()
+    })
+
+    it('can emit notification:downloadFailed:retryButton:clicked event', async () => {
+      const handler = handleNotificationButtonClicked(eventPublisher)
+      await handler(makeDownloadFailedNotificationId(100), 1)
+
+      expect(mockHandleDownloadFailedNotificationButtonClicked).toHaveBeenCalled()
+    })
+  })
+})
---
+++ src/serviceWorker/handlers/handleNotificationButtonClicked.ts
@@ -0,0 +1,62 @@
+import type { DomainEventPublisher } from '#domain/eventPublisher'
+import DownloadFailedNotificationRetryButtonClicked from '#domain/events/DownloadFailedNotificationRetryButtonClicked'
+import DownloadFailedNotificationViewButtonClicked from '#domain/events/DownloadFailedNotificationViewButtonClicked'
+import TweetFetchErrorNotificationViewButtonClicked from '#domain/events/TweetFetchErrorNotificationViewButtonClicked'
+import { UnknownNotificationButtonClicked } from '#domain/events/UnknownNotificationButtonClicked'
+import {
+  GeneralTweetFetchErrorNotificationButton,
+  MediaDownloadNotificationErrorButton,
+} from '#helpers/notificationConfig'
+import {
+  extractDownloadId,
+  extractTweetId,
+  isDownloadId,
+  isTweetFetchId,
+} from '#helpers/notificationId'
+
+const handleNotificationButtonClicked =
+  (publisher: DomainEventPublisher) =>
+  async (notificationId: string, buttonIndex: number) => {
+    if (isTweetFetchId(notificationId)) {
+      const tweetId = extractTweetId(notificationId)
+
+      let event: IDomainEvent
+      switch (buttonIndex) {
+        case GeneralTweetFetchErrorNotificationButton.ViewTweet:
+          event = new TweetFetchErrorNotificationViewButtonClicked(tweetId)
+          break
+
+        default:
+          event = new UnknownNotificationButtonClicked(buttonIndex)
+          break
+      }
+
+      publisher.publish(event)
+
+      return
+    }
+
+    if (isDownloadId(notificationId)) {
+      const downloadId = extractDownloadId(notificationId)
+
+      let event: IDomainEvent
+      switch (buttonIndex) {
+        case MediaDownloadNotificationErrorButton.RetryDownload:
+          event = new DownloadFailedNotificationRetryButtonClicked(downloadId)
+          break
+
+        case MediaDownloadNotificationErrorButton.ViewTweet:
+          event = new DownloadFailedNotificationViewButtonClicked(downloadId)
+          break
+
+        default:
+          event = new UnknownNotificationButtonClicked(buttonIndex)
+          break
+      }
+
+      publisher.publish(event)
+      return
+    }
+  }
+
+export default handleNotificationButtonClicked
