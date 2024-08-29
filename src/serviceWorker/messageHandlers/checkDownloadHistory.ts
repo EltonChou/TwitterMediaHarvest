13c814a9c60d021f5f01c77b353aff872b434036
Elton Chou <plscd748@gmail.com>
2024-08-30 01:05:08 +0800

    refactor: Implement message handlers.

---
+++ src/serviceWorker/messageHandlers/checkDownloadHistory.ts
@@ -0,0 +1,27 @@
+import { CheckDownloadHistoryMessage } from '#libs/webExtMessage'
+import { type MessageContextHandler, makeErrorResponse } from '../messageRouter'
+import {
+  CheckMediaTweetHasBeenDownloaded,
+  type InfraProvider,
+} from 'applicationUseCases/checkMediaTweetHasBeenDownloaded'
+
+const checkDownloadHistoryHandler = (
+  infraProvider: InfraProvider
+): MessageContextHandler => {
+  const checkMediaTweetHasBeenDownloaded = new CheckMediaTweetHasBeenDownloaded(
+    infraProvider
+  )
+
+  return async ctx => {
+    const { value: message, error } = CheckDownloadHistoryMessage.validate(ctx.message)
+    if (error) return ctx.response(makeErrorResponse(error.message))
+
+    const isExist = await checkMediaTweetHasBeenDownloaded.process({
+      tweetId: message.payload.tweetId,
+    })
+
+    return ctx.response(message.makeResponse(true, { isExist }))
+  }
+}
+
+export default checkDownloadHistoryHandler
---
+++ src/serviceWorker/messageHandlers/downloadMediaHandler.ts
@@ -0,0 +1,28 @@
+import { TweetInfo } from '#domain/valueObjects/tweetInfo'
+import { DownloadTweetMediaMessage } from '#libs/webExtMessage'
+import {
+  DownloadTweetMedia,
+  type InfraProvider,
+} from '../../applicationUseCases/downloadTweetMedia'
+import { type MessageContextHandler, makeErrorResponse } from '../messageRouter'
+
+const downloadMessageHandler = (infraProvider: InfraProvider): MessageContextHandler => {
+  const downloadTweetMedia = new DownloadTweetMedia(infraProvider)
+
+  return async ctx => {
+    const { value: message, error } = DownloadTweetMediaMessage.validate(ctx.message)
+    if (error) return ctx.response(makeErrorResponse(error.message))
+
+    const isOk = await downloadTweetMedia.process({
+      tweetInfo: new TweetInfo(message.payload),
+    })
+
+    return ctx.response(
+      isOk
+        ? message.makeResponse(isOk)
+        : message.makeResponse(isOk, 'Failed to complete download task.')
+    )
+  }
+}
+
+export default downloadMessageHandler
