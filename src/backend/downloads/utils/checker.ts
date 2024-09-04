c62acd1cd6fcf9491f0a13b7c5eb3c70731a82bb
Elton Chou <plscd748@gmail.com>
2024-09-04 22:34:31 +0800

    refactor: Remove legacy code.

--- src/backend/downloads/utils/checker.ts
+++
@@ -1,6 +0,0 @@
-export const isValidTweetMediaFileUrl = (url: string): boolean => {
-  const twitter_media_url_pattern =
-    /^https:\/\/(?:pbs|video)\.twimg\.com\/(?:media|.*_video.*)\/.*\.(?:jpg|png|gif|mp4)$/
-
-  return Boolean(url.match(twitter_media_url_pattern))
-}
