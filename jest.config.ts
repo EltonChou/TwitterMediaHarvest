9365e487089e373d22ba518833e0a85e38856979
Elton Chou <plscd748@gmail.com>
2024-11-15 02:20:33 +0800

    build: tune configurations.

--- jest.config.ts
+++ jest.config.ts
@@ -1,37 +1,30 @@
+import { compilerOptions } from './tsconfig.json'
 import type { JestConfigWithTsJest } from 'ts-jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
 
 const jestConfig: JestConfigWithTsJest = {
+  workerIdleMemoryLimit: '256',
   preset: 'ts-jest/presets/default-esm',
-  verbose: true,
+  // projects: ['src/pages'],
+  verbose: false,
   testTimeout: 5000,
   transform: {
     '\\.[jt]sx?$': [
       'ts-jest',
       {
         useESM: true,
+        isolatedModules: true,
       },
     ],
-    '^.+\\.svg$': 'jest-transformer-svg',
   },
-  transformIgnorePatterns: [
-    '/!node_modules\\/select-dom/',
-    '/!node_modules\\/@faker-js\\/faker/',
-  ],
+  transformIgnorePatterns: ['/!node_modules\\/@faker-js\\/faker/'],
   setupFiles: ['fake-indexeddb/auto', 'jest-webextension-mock'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
   roots: ['<rootDir>'],
-  moduleNameMapper: {
-    '#libs/(.*)$': '<rootDir>/src/libs/$1',
-    '#backend/(.*)$': '<rootDir>/src/backend/$1',
-    '#domain/(.*)$': '<rootDir>/src/domain/$1',
-    '#helpers/(.*)$': '<rootDir>/src/helpers/$1',
-    '#enums/(.*)$': '<rootDir>/src/enums/$1',
-    '#infra/(.*)$': '<rootDir>/src/infra/$1',
-    '#utils/(.*)$': '<rootDir>/src/utils/$1',
-    '#eventHandlers/(.*)$': '<rootDir>/src/eventHandlers/$1',
-    '#mocks/(.*)$': '<rootDir>/src/mocks/$1',
-    '#pages/(.*)$': '<rootDir>/src/pages/$1',
-  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    useESM: true,
+    prefix: '<rootDir>/src',
+  }),
   coveragePathIgnorePatterns: ['.mock.ts', '.mock.js', 'mocks', 'utils/test/*'],
 }
 
