2a247c05ad65bad308b62b1eeed5f63bfc8bf5c5
Elton Chou <plscd748@gmail.com>
2024-12-16 02:05:06 +0800

    build: Modify manifest.json when bundling

--- webpack.service.config.js
+++ webpack.service.config.js
@@ -3,30 +3,18 @@ const path = require('path')
 const baseConfig = require('./webpack.common.config')
 const { merge } = require('webpack-merge')
 const CopyPlugin = require('copy-webpack-plugin')
-const { LimitChunkCountPlugin } = require('webpack').optimize
+const PACKAGE = require('./package.json')
 
 module.exports = (env, argv) => {
+  const VERSION = PACKAGE.version
+  const BROWSER = env.target.split('-')[0]
+  const VERSION_NAME = `${VERSION} (${BROWSER})`
+
   return merge(baseConfig(env, argv), {
     name: 'service',
     output: {
-      chunkFormat: false
+      chunkFormat: false,
     },
-    // optimization: {
-    //   splitChunks: {
-    //     cacheGroups: {
-    //       defaultVendors: {
-    //         test: /[\\/]node_modules[\\/]/,
-    //         name: 'vendors-npm',
-    //         chunks: 'all',
-    //       },
-    //       commons: {
-    //         test: /[\\/]src[\\/](domain|enums|helpers|libs|mappers|utils|applicationUseCases|infra|provider)[\\/]/,
-    //         name: 'vendors-common',
-    //         chunks: 'all',
-    //       },
-    //     },
-    //   },
-    // },
     entry: {
       sw: path.resolve('./src/serviceWorker/sw.ts'),
       pages: path.resolve('./src/pages/index.tsx'),
@@ -40,13 +28,20 @@ module.exports = (env, argv) => {
       ],
     },
     plugins: [
-      new LimitChunkCountPlugin({ maxChunks: 1 }),
       new CopyPlugin({
         patterns: [
           {
             from: 'manifest.json',
             context: 'src',
             to: '[name][ext]',
+            transform: content => {
+              let contentStr = content
+                .toString()
+                .replace('__MANIFEST_RELEASE_VERSION__', VERSION)
+                .replace('__MANIFEST_VERSION_NAME__', VERSION_NAME)
+
+              return Buffer.from(contentStr)
+            },
           },
           {
             from: 'assets/icons/*.png',
