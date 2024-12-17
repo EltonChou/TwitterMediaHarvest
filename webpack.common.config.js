39827949ea80d1d379e67546b2aa2cc3f0961a5d
Elton Chou <plscd748@gmail.com>
2024-12-18 01:54:30 +0800

    build: Remove unused dependencies

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -18,11 +18,8 @@ const config = {
     },
     fallback: {
       path: require.resolve('path-browserify'),
-      // crypto: require.resolve('crypto-browserify'),
-      // stream: require.resolve('stream-browserify'),
-      // vm: require.resolve('vm-browserify'),
     },
-    plugins: [/* new NodePolyfillPlugin(), */ new TsconfigPathsPlugin()],
+    plugins: [new TsconfigPathsPlugin()],
   },
   output: {
     filename: '[name].js',
