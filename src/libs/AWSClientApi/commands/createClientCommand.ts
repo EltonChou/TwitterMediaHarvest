554c6b56474b1355473757e8c10e622e8e581c92
Elton Chou <plscd748@gmail.com>
2024-12-18 00:38:32 +0800

    refactor: Use jwt-decode for decoding jwt
    Since client only need to decode jwt, we don't need a rich lib.
    
    jwt-decode is jwt decoding library for browser.
    It can prevents bundled code introducing too many polyfill libs.

--- src/libs/AWSClientApi/commands/createClientCommand.ts
+++ src/libs/AWSClientApi/commands/createClientCommand.ts
@@ -4,7 +4,15 @@ import { CommandResponseError } from './errors'
 import { HttpMethod, type MetadataBearer, type RequestContext } from './types'
 import { streamCollector } from '@smithy/fetch-http-handler'
 import { HttpRequest, type HttpResponse } from '@smithy/protocol-http'
-import jws from 'jws'
+import { jwtDecode } from 'jwt-decode'
+import type { JwtPayload } from 'jwt-decode'
+
+interface TokenPayload extends JwtPayload {
+  uuid: string
+  uninstallCode: string
+  trafficUsage: number
+  downloadCount: number
+}
 
 interface UsageStatistics {
   downloadCount: number
@@ -48,18 +56,20 @@ export class CreateClientCommand extends BaseCommand<
     const metadataBearer = responseToMetadataBearer(response)
 
     if (response.statusCode === 201) {
-      const signature = jws.decode(body.token, { json: true })
-      if (!signature)
-        throw new CommandResponseError(
-          body?.error ?? body?.message ?? 'Failed to decode response body.',
-          metadataBearer
-        )
+      try {
+        const decodedToken = jwtDecode<TokenPayload>(body.token)
+        return {
+          ...metadataBearer,
+          syncToken: body.token,
+          clientUUID: decodedToken['uuid'],
+          uninstallCode: body.uninstallCode ?? decodedToken['uninstallCode'],
+        }
+      } catch (error) {
+        let errorMessage: string = body?.error ?? body?.message
+        if (error instanceof Error) errorMessage = error.message
+        errorMessage ??= 'Failed to decode response body.'
 
-      return {
-        ...metadataBearer,
-        syncToken: body.token,
-        clientUUID: signature.payload['uuid'],
-        uninstallCode: body.uninstallCode ?? signature.payload['uninstallCode'],
+        throw new CommandResponseError(errorMessage, metadataBearer)
       }
     }
 
