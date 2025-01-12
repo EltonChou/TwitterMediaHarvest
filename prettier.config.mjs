24415f1a7bda32be008de36b1dfb94096d6213fc
-h <plscd748@gmail.com>
2025-01-12 21:01:18 +0800

    style: Use shared config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,18 +1,3 @@
-/**
- * @see https://prettier.io/docs/en/configuration.html
- * @type {import("prettier").Config}
- */
-const config = {
-  trailingComma: 'es5',
-  semi: false,
-  singleQuote: true,
-  bracketSpacing: true,
-  arrowParens: 'avoid',
-  endOfLine: 'lf',
-  printWidth: 80,
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
-}
+import config from '@media-harvest-config/prettier'
 
 export default config
