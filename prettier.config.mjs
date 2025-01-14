4b76042fc094c3f857f035f84479e4bbf1851a95
Elton H.Y. Chou <plscd748@gmail.com>
2025-01-15 02:32:59 +0800

    style: Remove prettier json override and format json

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,14 +3,6 @@ import baseConfig from '@media-harvest-config/prettier'
 /** @type {import('prettier').Config} */
 const config = {
   ...baseConfig,
-  overrides: [
-    {
-      files: '*.json',
-      options: {
-        tabWidth: 4,
-      },
-    },
-  ],
 }
 
 export default config
