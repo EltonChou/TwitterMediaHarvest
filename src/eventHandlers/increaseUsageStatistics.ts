a5cd9d39c8f67edfffee237e8a73664cce5e24a5
-h <plscd748@gmail.com>
2025-01-14 17:23:50 +0800

    refactor: Improve readability

--- src/eventHandlers/increaseUsageStatistics.ts
+++ src/eventHandlers/increaseUsageStatistics.ts
@@ -1,6 +1,7 @@
-import { DomainEventHandler } from '#domain/eventPublisher'
-import { IDownloadRepository } from '#domain/repositories/download'
-import { IUsageStatisticsRepository } from '#domain/repositories/usageStatistics'
+import type { DomainEventHandler } from '#domain/eventPublisher'
+import type { IDownloadRepository } from '#domain/repositories/download'
+import type { IUsageStatisticsRepository } from '#domain/repositories/usageStatistics'
+import type { UsageStatistics } from '#domain/valueObjects/usageStatistics'
 
 export const increaseUsageStatistics =
   (
@@ -8,22 +9,23 @@ export const increaseUsageStatistics =
     downloadRepo: IDownloadRepository
   ): DomainEventHandler<DownloadEvent | IDomainEvent> =>
   async event => {
+    let newStats: UsageStatistics
+
+    const stats = await statsRepo.get()
     if (assertDownloadEvent(event)) {
       const item = await downloadRepo.getById(event.downloadId)
-      const stats = await statsRepo.get()
-      const newStats = stats.increase({
+      newStats = stats.increase({
         downloadCount: 1,
         trafficUsage: item ? item.fileSize : 0,
       })
-      await statsRepo.save(newStats)
     } else {
-      const stats = await statsRepo.get()
-      const newStats = stats.increase({
+      newStats = stats.increase({
         downloadCount: 1,
         trafficUsage: 0,
       })
-      await statsRepo.save(newStats)
     }
+
+    await statsRepo.save(newStats)
   }
 
 const assertDownloadEvent: Assert<IDomainEvent, DownloadEvent> = (
