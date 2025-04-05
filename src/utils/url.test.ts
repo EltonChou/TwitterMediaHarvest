f926802c05823be0bf23918452764cfc01cf57e5
Elton Chou <plscd748@gmail.com>
2025-04-06 03:13:28 +0800

    fix: Correct URL validation logic in isObjectUrl function

--- src/utils/url.test.ts
+++ src/utils/url.test.ts
@@ -4,6 +4,8 @@ test.each([
   { url: 'https://mediaharvest.app/xxx.json', expected: false },
   { url: 'blob:https://somewhre/some-uuid', expected: true },
   { url: 'file://storage/xxx.json', expected: false },
+  { url: '', expected: false },
+  { url: 'blob', expected: false },
 ])('$url is object url: $expect', ({ url, expected }) => {
   expect(isObjectUrl(url)).toBe(expected)
 })
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,7 +1,7 @@
 export type ObjectUrl = string
 
 export const isObjectUrl = (url: string): url is ObjectUrl => {
-  if (!url && !URL.canParse(url)) return false
+  if (!url || !URL.canParse(url)) return false
 
   const parsedUrl = URL.parse(url)
   return parsedUrl ? parsedUrl.protocol === 'blob:' : false
