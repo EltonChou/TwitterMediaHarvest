f3a3c48ef05198f14b8515559a49715101512949
Elton Chou <plscd748@gmail.com>
2025-04-08 00:46:08 +0800

    license: Add MPL-2.0 header

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 const SECOND = 1000
 const MINUTE = 60 * SECOND
 const HOUR = 60 * MINUTE
