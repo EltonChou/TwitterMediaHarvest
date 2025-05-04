2931f1fdca5a614bec6e2e54fc30773bce357b6e
Elton Chou <plscd748@gmail.com>
2025-05-05 00:22:22 +0800

    license: add MPL license header

--- src/applicationUseCases/captureTransactionId.ts
+++ src/applicationUseCases/captureTransactionId.ts
@@ -1,3 +1,8 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import type { IXTransactionIdRepository } from '#domain/repositories/xTransactionId'
 import type { AsyncUseCase } from '#domain/useCases/base'
 import { XTransactionId } from '#domain/valueObjects/xTransactionId'
