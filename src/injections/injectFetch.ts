337b92aaaf480bb9f9f36fea1dda9b5f76a3af0f
Elton Chou <plscd748@gmail.com>
2025-05-10 15:21:53 +0800

    feat: Extract transaction id generator

--- src/injections/injectFetch.ts
+++ src/injections/injectFetch.ts
@@ -4,8 +4,27 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+export {}
+
+declare global {
+  interface Window {
+    webpackChunk_twitter_responsive_web: WebPackModuleItem[]
+  }
+}
+
+type WebpackLoadFunction = (a: unknown, b: unknown, c: unknown) => void
+type Module = Record<number | string, WebpackLoadFunction>
+type WebPackModuleItem = [[string], Module]
+type ESModule<T = unknown> = {
+  default: T
+  __esModule: true
+}
+type MakeTransactionId = (path: string, method: string) => Promise<string>
+
 const xOpen = XMLHttpRequest.prototype.open
 const xSetHeader = XMLHttpRequest.prototype.setRequestHeader
+
+let generateTransactionId: MakeTransactionId
 
 type TxTarget = {
   method: string
@@ -21,7 +40,10 @@ const Pattern = Object.freeze({
 
 const enum MediaHarvestEvent {
   MediaResponse = 'mh:media-response',
-  TransactionId = 'mh:tx-id',
+  CaptureTransactionId = 'mh:tx-id:capture',
+  ResponseTransactionId = 'mh:tx-id:response',
+  RequestTransactionId = 'mh:tx-id:request',
+  QueryString = 'mh:query-string',
 }
 
 XMLHttpRequest.prototype.setRequestHeader = function (
@@ -92,3 +114,116 @@ function captureResponse(this: XMLHttpRequest, _ev: ProgressEvent) {
     document.dispatchEvent(event)
   }
 }
+
+self.webpackChunk_twitter_responsive_web = new Proxy<
+  Window['webpackChunk_twitter_responsive_web']
+>([], {
+  get: function (target, prop, receiver) {
+    return prop === 'push'
+      ? arrayPushProxy(target.push.bind(target))
+      : Reflect.get(target, prop, receiver)
+  },
+})
+
+function arrayPushProxy<T>(arrayPush: Array<T>['push']) {
+  return new Proxy(arrayPush, {
+    apply(method, thisArg, args: WebPackModuleItem[]) {
+      return Reflect.apply(
+        method,
+        thisArg,
+        args.map(item => {
+          const [[name], module] = item
+          return name.includes('ondemand.s')
+            ? [[name], moduleProxy(module)]
+            : item
+        })
+      )
+    },
+  })
+}
+
+function moduleProxy(module: Module) {
+  return new Proxy(module, {
+    get(target, prop, receiver) {
+      return typeof prop === 'symbol'
+        ? Reflect.get(target, prop, receiver)
+        : webpackLoaderFunctionProxy(target[prop])
+    },
+  })
+}
+
+function esModuleProxy(esModule: Partial<ESModule>) {
+  return new Proxy(esModule, {
+    defineProperty(target, property, attributes) {
+      if (property === 'default')
+        return Reflect.defineProperty(target, property, {
+          ...attributes,
+          configurable: true,
+        })
+
+      return Reflect.defineProperty(target, property, attributes)
+    },
+  })
+}
+
+function webpackLoaderFunctionProxy(loaderFunc: WebpackLoadFunction) {
+  return new Proxy(loaderFunc, {
+    apply(
+      exportItem,
+      thisArg,
+      args: [object, Partial<ESModule>, CallableFunction]
+    ) {
+      const [_, esModule, loader] = args
+      const returnVal = Reflect.apply(exportItem, thisArg, [
+        _,
+        esModuleProxy(esModule),
+        loader,
+      ])
+
+      if (
+        isESModule(esModule) &&
+        isCallableFunction<() => MakeTransactionId>(esModule.default)
+      ) {
+        const txIdGenerator = esModule.default()
+        generateTransactionId ||= txIdGenerator
+        Object.defineProperty(esModule, 'default', {
+          configurable: true,
+          enumerable: true,
+          get: () => () => txIdGenerator,
+        })
+      }
+
+      return returnVal
+    },
+  })
+}
+
+function isESModule(value: unknown): value is ESModule {
+  return (
+    typeof value === 'object' &&
+    value !== null &&
+    '__esModule' in value &&
+    value.__esModule === true
+  )
+}
+
+function isCallableFunction<T>(value: unknown): value is T {
+  return typeof value === 'function'
+}
+
+document.addEventListener('mh:tx-id:request', async e => {
+  const { path, method, uuid } = e.detail
+  const txId = await generateTransactionId(path, method)
+
+  document.dispatchEvent(
+    new CustomEvent<MediaHarvest.TxIdResponseDetail>(
+      MediaHarvestEvent.ResponseTransactionId,
+      {
+        detail: {
+          uuid,
+          value: txId,
+        },
+      }
+    )
+  )
+})
